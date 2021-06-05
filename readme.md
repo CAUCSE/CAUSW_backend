7bde39eb0f20d7d1299545d8ca8cf117437dc8c2
Gunhee Jang <ghj5719@naver.com>
2021-06-05 15:41:45 +0900

    Init & Skel of server

---
+++ readme.md
@@ -0,0 +1,6 @@
+# CAUSW Backend
+
+### Environment
+
+- JDK 11.0.9
+- Spring Boot 2.4.1
---
+++ src/main/java/net/causw/CauswApplication.java
@@ -0,0 +1,11 @@
+package net.causw;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CauswApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(CauswApplication.class, args);
+    }
+}
