c75f574fee589a52d051c4e36ec29c8d0d35aa50
kyoungchan <dlsrks1021@gmail.com>
2021-07-06 21:41:02 +0900

    chore: Swagger 적용

---
+++ src/main/java/net/causw/config/SwaggerConfig.java
@@ -0,0 +1,38 @@
+package net.causw.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+@EnableSwagger2
+@Configuration
+public class SwaggerConfig {
+
+    private static final String API_NAME = "CAU_SW API";
+    private static final String API_VERSION = "0.0.1";
+    private static final String API_DESCRIPTION = "중앙대학교 소프트웨어학부 동문네트워크 API";
+
+    @Bean
+    public Docket api() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .select()
+                .apis(RequestHandlerSelectors.basePackage("net.causw"))
+                .paths(PathSelectors.any())
+                .build()
+                .apiInfo(apiInfo());
+    }
+
+    public ApiInfo apiInfo() {
+        return new ApiInfoBuilder()
+                .title(API_NAME)
+                .version(API_VERSION)
+                .description(API_DESCRIPTION)
+                .build();
+    }
+}
