0fb7ff7c10b27ab72498636e4f88f22dd915d416
SuHyun Kyung <ase3238@gmail.com>
2021-07-11 18:26:57 +0900

    fix: import 와일드카드 처리 수정

--- src/main/java/net/causw/web/LockerController.java
+++ src/main/java/net/causw/web/LockerController.java
@@ -3,7 +3,11 @@ package net.causw.web;
 import net.causw.application.LockerService;
 import net.causw.application.dto.LockerDetailDto;
 import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api/v1/locker")
