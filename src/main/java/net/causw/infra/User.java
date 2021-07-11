fd798a8ee4815ddb8d8ffcb709858907df11f302
SuHyun Kyung <ase3238@gmail.com>
2021-07-11 18:26:57 +0900

    fix: import 와일드카드 처리 수정

--- src/main/java/net/causw/infra/User.java
+++ src/main/java/net/causw/infra/User.java
@@ -3,7 +3,14 @@ package net.causw.infra;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
-import javax.persistence.*;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToOne;
+import javax.persistence.Table;
+
 
 @Getter
 @Entity
