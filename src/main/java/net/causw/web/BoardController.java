d05c8c67a16996119ea6dd9470bd75106f4251ef
als95 <als950901@gmail.com>
2021-07-19 20:21:59 +0900

    refac: Fix url

--- src/main/java/net/causw/web/BoardController.java
+++ src/main/java/net/causw/web/BoardController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/api/v1/board")
+@RequestMapping("/api/v1/boards")
 public class BoardController {
     private final BoardService boardService;
 
--- src/main/java/net/causw/web/PostController.java
+++ src/main/java/net/causw/web/PostController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/api/v1/post")
+@RequestMapping("/api/v1/posts")
 public class PostController {
     private final PostService postService;
 
