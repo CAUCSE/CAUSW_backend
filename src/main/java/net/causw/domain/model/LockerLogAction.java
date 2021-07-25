24a54a1993935449ab6b189c163545fca69a2d91
SuHyun Kyung <ase3238@gmail.com>
2021-07-25 18:09:06 +0900

    fix: enum 데이터 변경

--- src/main/java/net/causw/domain/model/LockerLogAction.java
+++ src/main/java/net/causw/domain/model/LockerLogAction.java
@@ -1,10 +1,10 @@
 package net.causw.domain.model;
 
 public enum LockerLogAction {
-    ENABLE("활성화"),
-    DISABLE("비활성화"),
-    REGISTER("신청"),
-    RETURN("반납");
+    ENABLE("enable"),
+    DISABLE("disable"),
+    REGISTER("register"),
+    RETURN("return");
 
     private String value;
 
