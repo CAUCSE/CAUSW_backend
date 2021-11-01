7e6f9b84cd25001c09ad09c55674e8b73175202d
jhcheon <jhcheon1000@github.com>
2021-11-01 19:26:02 +0900

    resolve conflict

--- src/main/java/net/causw/adapter/web/CommentController.java
+++ src/main/java/net/causw/adapter/web/CommentController.java
@@ -6,6 +6,7 @@ import net.causw.application.dto.CommentResponseDto;
 import org.springframework.data.domain.Page;
 import org.springframework.http.HttpStatus;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -47,8 +48,8 @@ public class CommentController {
     @ResponseStatus(value = HttpStatus.OK)
     public CommentResponseDto delete(
             @AuthenticationPrincipal String userId,
-            @PathVariable String commentId
+            @PathVariable String id
     ) {
-        return this.commentService.delete(userId, commentId);
+        return this.commentService.delete(userId, id);
     }
 }
