a7348c3d945f2fcc90ad2fc2a6a6bbadeb481e43
Gunhee Jang <ghj5719@naver.com>
2021-11-28 20:20:58 +0900

    feat: set time zone to UTC+9

--- src/main/java/net/causw/CauswApplication.java
+++ src/main/java/net/causw/CauswApplication.java
@@ -4,10 +4,18 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+import javax.annotation.PostConstruct;
+import java.util.TimeZone;
+
 @SpringBootApplication
 @EnableJpaAuditing
 public class CauswApplication {
     public static void main(String[] args) {
         SpringApplication.run(CauswApplication.class, args);
+    }
+
+    @PostConstruct
+    public void init() {
+        TimeZone.setDefault(TimeZone.getTimeZone("Asia/Seoul"));
     }
 }
