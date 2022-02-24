e06217a7b91a5fa4ca80996639ba35321e5e310b
Minsoo <37828653+als95@users.noreply.github.com>
2022-02-24 17:49:51 +0900

    refac: Add President user give Leader Circle role (#314)

--- src/main/java/net/causw/application/dto/UserUpdateRoleRequestDto.java
+++ src/main/java/net/causw/application/dto/UserUpdateRoleRequestDto.java
@@ -6,14 +6,20 @@ import lombok.NoArgsConstructor;
 import lombok.Setter;
 import net.causw.domain.model.Role;
 
+import java.util.Optional;
+
 @Getter
 @Setter
 @AllArgsConstructor
 @NoArgsConstructor
 public class UserUpdateRoleRequestDto {
     private String role;
+    private String circleId;
 
     public Role getRole() {
         return Role.of(this.role);
+    }
+    public Optional<String> getCircleId() {
+        return Optional.ofNullable(this.circleId);
     }
 }
