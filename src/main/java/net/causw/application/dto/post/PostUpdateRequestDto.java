bf0a6d27845783e59ed1b025622d6cb4171313ec
Minsoo <37828653+als95@users.noreply.github.com>
2022-03-23 20:21:48 +0900

    fix: Fix null pointer exception of post attachments (#372)

--- src/main/java/net/causw/application/dto/post/PostUpdateRequestDto.java
+++ src/main/java/net/causw/application/dto/post/PostUpdateRequestDto.java
@@ -6,6 +6,7 @@ import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import java.util.List;
+import java.util.Optional;
 
 @Getter
 @Setter
@@ -15,4 +16,8 @@ public class PostUpdateRequestDto {
     private String title;
     private String content;
     private List<String> attachmentList;
+
+    public List<String> getAttachmentList() {
+        return Optional.ofNullable(this.attachmentList).orElse(List.of());
+    }
 }
