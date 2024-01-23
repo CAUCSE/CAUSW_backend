22e3219ac45c93948daf79322a26f9548ad575d3
SL313 <rebin0313@gmail.com>
2024-01-23 22:16:37 +0900

    feat : UserSignInRequestDto 스웨거 설정

--- src/main/java/net/causw/application/dto/user/UserSignInRequestDto.java
+++ src/main/java/net/causw/application/dto/user/UserSignInRequestDto.java
@@ -1,5 +1,6 @@
 package net.causw.application.dto.user;
 
+import io.swagger.annotations.*;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -10,6 +11,10 @@ import lombok.Setter;
 @AllArgsConstructor
 @NoArgsConstructor
 public class UserSignInRequestDto {
+
+    @ApiModelProperty(value = "이메일", example = "yebin@cau.ac.kr")
     private String email;
+
+    @ApiModelProperty(value = "비밀번호", example = "password00!!")
     private String password;
 }
