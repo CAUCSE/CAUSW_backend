b533e469c2f0e3e6db81e72238551d50e03b2ef6
SL313 <rebin0313@gmail.com>
2024-01-24 10:12:15 +0900

    feat : 승인 req res DTO 스웨거 설정

--- src/main/java/net/causw/application/dto/user/UserAdmissionCreateRequestDto.java
+++ src/main/java/net/causw/application/dto/user/UserAdmissionCreateRequestDto.java
@@ -1,5 +1,6 @@
 package net.causw.application.dto.user;
 
+import io.swagger.annotations.*;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -13,8 +14,14 @@ import java.util.Optional;
 @AllArgsConstructor
 @NoArgsConstructor
 public class UserAdmissionCreateRequestDto {
+
+    @ApiModelProperty(value = "이메일", example = "yebin@cau.ac.kr")
     private String email;
+
+    @ApiModelProperty(value = "자기소개 글 (255자 이내)", example = "안녕하세요! 코딩을 좋아하는 신입생 이예빈입니다.")
     private String description;
+
+    @ApiModelProperty(value = "이미지", example = "")
     private MultipartFile attachImage;
 
     public Optional<MultipartFile> getAttachImage() {
--- src/main/java/net/causw/application/dto/user/UserAdmissionResponseDto.java
+++ src/main/java/net/causw/application/dto/user/UserAdmissionResponseDto.java
@@ -12,13 +12,19 @@ import java.time.LocalDateTime;
 @Setter
 public class UserAdmissionResponseDto {
 
-    @ApiModelProperty(value = "admission id", example = "ff7576321d26d71c018d26dc284a9999", required = true)
+    @ApiModelProperty(value = "승인 고유 id 값", example = "uuid 형식의 String 값입니다.", required = true)
     private String id;
 
     private UserResponseDto user;
     private String attachImage;
+
+    @ApiModelProperty(value = "자기소개 글 (255자 이내)", example = "안녕하세요! 코딩을 좋아하는 신입생 이예빈입니다.")
     private String description;
+
+    @ApiModelProperty(value = "생선된 시각", example = "2024-01-24T00:26:40.643Z")
     private LocalDateTime createdAt;
+
+    @ApiModelProperty(value = "마지막 업데이트된 시각", example = "2024-01-24T00:26:40.643Z")
     private LocalDateTime updatedAt;
 
     public UserAdmissionResponseDto(
