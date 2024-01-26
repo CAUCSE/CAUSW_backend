ee3cc5bdd2329c3d4bcad39e608101ba60d8f862
pstar987 <pstar987@naver.com>
2024-01-26 17:58:57 +0900

    feat : BoardCreateRequestDto 스웨거 설정

--- src/main/java/net/causw/application/dto/board/BoardCreateRequestDto.java
+++ src/main/java/net/causw/application/dto/board/BoardCreateRequestDto.java
@@ -1,5 +1,6 @@
 package net.causw.application.dto.board;
 
+import io.swagger.annotations.ApiModelProperty;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -13,11 +14,20 @@ import java.util.Optional;
 @AllArgsConstructor
 @NoArgsConstructor
 public class BoardCreateRequestDto {
+
+    @ApiModelProperty(value = "Board 이름", example = "board_name")
     private String name;
+
+    @ApiModelProperty(value = "Board 설명", example = "board_description")
     private String description;
+
+    @ApiModelProperty(value = "createRoleList(미완)", example = "ADMIN")
     private List<String> createRoleList;
+
+    @ApiModelProperty(value = "Board 카테고리", example = "APP_NOTICE")
     private String category;
 
+    @ApiModelProperty(value = "속한 동아리 id", example = "circleId_example")
     private String circleId;
 
     public Optional<String> getCircleId() {
