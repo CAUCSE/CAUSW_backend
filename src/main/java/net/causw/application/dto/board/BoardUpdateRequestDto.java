fd2b25107c4d602c54aec06322052aab13bc6fc5
pstar987 <pstar987@naver.com>
2024-01-26 17:59:50 +0900

    feat : BoardUpdateRequestDto 스웨거 설정

--- src/main/java/net/causw/application/dto/board/BoardUpdateRequestDto.java
+++ src/main/java/net/causw/application/dto/board/BoardUpdateRequestDto.java
@@ -1,5 +1,6 @@
 package net.causw.application.dto.board;
 
+import io.swagger.annotations.ApiModelProperty;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -12,8 +13,17 @@ import java.util.List;
 @AllArgsConstructor
 @NoArgsConstructor
 public class BoardUpdateRequestDto {
+
+    @ApiModelProperty(value = "Board 이름", example = "board_example")
     private String name;
+
+    @ApiModelProperty(value = "Board 설명", example = "board_description")
     private String description;
+
+
+    @ApiModelProperty(value = "createRoleList(미완)", example = "ADMIN")
     private List<String> createRoleList;
+
+    @ApiModelProperty(value = "Board 카테고리", example = "APP_NOTICE")
     private String category;
 }
