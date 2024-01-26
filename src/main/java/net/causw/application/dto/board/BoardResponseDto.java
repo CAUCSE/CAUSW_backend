58bdded972bdd228515ab3ece5db231b21f35f03
pstar987 <pstar987@naver.com>
2024-01-26 23:12:12 +0900

    update : BoardResponseDto 스웨거 설정(createRoleList의 example list로 수정)

--- src/main/java/net/causw/application/dto/board/BoardResponseDto.java
+++ src/main/java/net/causw/application/dto/board/BoardResponseDto.java
@@ -21,7 +21,7 @@ public class BoardResponseDto {
     @ApiModelProperty(value = "Board 설명", example = "board_description")
     private String description;
 
-    @ApiModelProperty(value = "createRoleList(미완)", example = "ADMIN")
+    @ApiModelProperty(value = "createRoleList(미완)", example = "[ 'ADMIN' ]")
     private List<String> createRoleList;
 
     @ApiModelProperty(value = "Board 카테고리", example = "APP_NOTICE")
