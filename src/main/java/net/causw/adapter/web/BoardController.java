fa797007c1b7816acbba81613a189ea11e95a8e9
pstar987 <pstar987@naver.com>
2024-01-26 22:49:17 +0900

    refac : swagger 상에서 usertoken header로 받기(board)

--- src/main/java/net/causw/adapter/web/BoardController.java
+++ src/main/java/net/causw/adapter/web/BoardController.java
@@ -1,20 +1,13 @@
 package net.causw.adapter.web;
 
+import io.swagger.annotations.ApiOperation;
 import net.causw.application.board.BoardService;
 import net.causw.application.dto.board.BoardCreateRequestDto;
 import net.causw.application.dto.board.BoardResponseDto;
 import net.causw.application.dto.board.BoardUpdateRequestDto;
 import org.springframework.http.HttpStatus;
-import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseStatus;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -29,44 +22,57 @@ public class BoardController {
 
     @GetMapping
     @ResponseStatus(value = HttpStatus.OK)
-    public List<BoardResponseDto> findAll(@AuthenticationPrincipal String userId) {
+    @ApiOperation(value = "게시판 검색 API (미완료/사용가능)", notes = "userId 값은 token 으로 적용되었습니다")
+    public List<BoardResponseDto> findAll() {
+        Object principal = SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+        String userId = ((String) principal);
         return this.boardService.findAll(userId);
     }
 
     @PostMapping
     @ResponseStatus(value = HttpStatus.CREATED)
+    @ApiOperation(value = "게시판 생성 API (미완료/사용가능)", notes = "circleId는 현재 존재하는 circleId를 적용해야 합니다")
     public BoardResponseDto create(
-            @AuthenticationPrincipal String creatorId,
             @RequestBody BoardCreateRequestDto boardCreateRequestDto
     ) {
+        Object principal = SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+        String creatorId = ((String) principal);
         return this.boardService.create(creatorId, boardCreateRequestDto);
     }
 
     @PutMapping(value = "/{id}")
     @ResponseStatus(value = HttpStatus.OK)
+    @ApiOperation(value = "게시판 업데이트 API (미완료/사용가능)", notes = "id 에는 board id 값을 넣어주세요")
     public BoardResponseDto update(
-            @AuthenticationPrincipal String updaterId,
             @PathVariable String id,
             @RequestBody BoardUpdateRequestDto boardUpdateRequestDto
     ) {
+        Object principal = SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+        String updaterId = ((String) principal);
         return this.boardService.update(updaterId, id, boardUpdateRequestDto);
     }
 
     @DeleteMapping(value = "/{id}")
     @ResponseStatus(value = HttpStatus.OK)
+    @ApiOperation(value = "게시판 삭제 API (미완료/사용가능)", notes = "id 에는 board id 값을 넣어주세요")
     public BoardResponseDto delete(
-            @AuthenticationPrincipal String deleterId,
+
             @PathVariable String id
     ) {
+        Object principal = SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+        String deleterId = ((String) principal);
         return this.boardService.delete(deleterId, id);
     }
 
     @PutMapping(value = "/{id}/restore")
     @ResponseStatus(value =  HttpStatus.OK)
+    @ApiOperation(value = "게시판 복구 API (미완료/사용가능)", notes = "id 에는 board id 값을 넣어주세요")
     public BoardResponseDto restore(
-            @AuthenticationPrincipal String restorerId,
+
             @PathVariable String id
     ){
+        Object principal = SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+        String restorerId = ((String) principal);
         return this.boardService.restore(restorerId, id);
     }
 }
