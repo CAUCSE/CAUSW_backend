65ae3cf497ab322d3f3b0fcc57f09ff949ca044d
pstar987 <pstar987@naver.com>
2024-01-28 18:06:42 +0900

    update : import문 wildcard 방식 -> 각 import 대상 지정

--- src/main/java/net/causw/adapter/web/BoardController.java
+++ src/main/java/net/causw/adapter/web/BoardController.java
@@ -7,7 +7,16 @@ import net.causw.application.dto.board.BoardResponseDto;
 import net.causw.application.dto.board.BoardUpdateRequestDto;
 import org.springframework.http.HttpStatus;
 import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.PathVariable;
+
 
 import java.util.List;
 
