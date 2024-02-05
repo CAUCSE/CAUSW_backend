ab6068de8c7916627e2762dd82e02698ba126ff3
pstar987 <pstar987@naver.com>
2024-02-05 16:15:47 +0900

    feat : id명 loginUserId로 통일

--- src/main/java/net/causw/adapter/web/PostController.java
+++ src/main/java/net/causw/adapter/web/PostController.java
@@ -6,7 +6,7 @@ import net.causw.application.dto.post.PostCreateRequestDto;
 import net.causw.application.dto.post.PostResponseDto;
 import net.causw.application.dto.post.PostUpdateRequestDto;
 import org.springframework.http.HttpStatus;
-import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -30,70 +30,77 @@ public class PostController {
     @GetMapping(value = "/{id}")
     @ResponseStatus(value = HttpStatus.OK)
     public PostResponseDto findById(
-            @AuthenticationPrincipal String requestUserId,
             @PathVariable String id
     ) {
-        return this.postService.findById(requestUserId, id);
+        Object principal = SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+        String loginUserId = ((String) principal);
+        return this.postService.findById(loginUserId, id);
     }
 
     @GetMapping
     @ResponseStatus(value = HttpStatus.OK)
     public BoardPostsResponseDto findAll(
-            @AuthenticationPrincipal String requestUserId,
             @RequestParam String boardId,
             @RequestParam(defaultValue = "0") Integer pageNum
     ) {
-        return this.postService.findAll(requestUserId, boardId, pageNum);
+        Object principal = SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+        String loginUserId = ((String) principal);
+        return this.postService.findAll(loginUserId, boardId, pageNum);
     }
 
     @GetMapping("/search")
     @ResponseStatus(value = HttpStatus.OK)
     public BoardPostsResponseDto search(
-            @AuthenticationPrincipal String requestUserId,
             @RequestParam String boardId,
             @RequestParam(defaultValue = "title") String option,
             @RequestParam(defaultValue = "") String keyword,
             @RequestParam(defaultValue = "0") Integer pageNum
     ) {
-        return this.postService.search(requestUserId, boardId, option, keyword, pageNum);
+        Object principal = SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+        String loginUserId = ((String) principal);
+        return this.postService.search(loginUserId, boardId, option, keyword, pageNum);
     }
 
     @GetMapping("/app/notice")
     @ResponseStatus(value = HttpStatus.OK)
     public BoardPostsResponseDto findAllAppNotice(
-            @AuthenticationPrincipal String requestUserId,
             @RequestParam(defaultValue = "0") Integer pageNum
     ) {
-        return this.postService.findAllAppNotice(requestUserId, pageNum);
+        Object principal = SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+        String loginUserId = ((String) principal);
+        return this.postService.findAllAppNotice(loginUserId, pageNum);
     }
 
     @PostMapping
     @ResponseStatus(value = HttpStatus.CREATED)
     public PostResponseDto create(
-            @AuthenticationPrincipal String requestUserId,
             @RequestBody PostCreateRequestDto postCreateRequestDto
     ) {
-        return this.postService.create(requestUserId, postCreateRequestDto);
+        Object principal = SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+        String loginUserId = ((String) principal);
+        return this.postService.create(loginUserId, postCreateRequestDto);
     }
 
     @DeleteMapping(value = "/{id}")
     @ResponseStatus(value = HttpStatus.OK)
     public PostResponseDto delete(
-            @AuthenticationPrincipal String requestUserId,
             @PathVariable String id
     ) {
-        return this.postService.delete(requestUserId, id);
+        Object principal = SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+        String loginUserId = ((String) principal);
+        return this.postService.delete(loginUserId, id);
     }
 
     @PutMapping(value = "/{id}")
     @ResponseStatus(value = HttpStatus.OK)
     public PostResponseDto update(
-            @AuthenticationPrincipal String requestUserId,
             @PathVariable String id,
             @RequestBody PostUpdateRequestDto postUpdateRequestDto
     ) {
+        Object principal = SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+        String loginUserId = ((String) principal);
         return this.postService.update(
-                requestUserId,
+                loginUserId,
                 id,
                 postUpdateRequestDto
         );
@@ -102,11 +109,12 @@ public class PostController {
     @PutMapping(value = "/{id}/restore")
     @ResponseStatus(value = HttpStatus.OK)
     public PostResponseDto restore(
-            @AuthenticationPrincipal String requestUserId,
             @PathVariable String id
     ) {
+        Object principal = SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+        String loginUserId = ((String) principal);
         return this.postService.restore(
-                requestUserId,
+                loginUserId,
                 id
         );
     }
