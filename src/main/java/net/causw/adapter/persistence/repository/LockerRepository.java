55fb441b623343a57efde004f86829decae5e5b4
realisshomyang <realisshoman@kakao.com>
2024-02-24 18:01:12 +0900

    feat : 사물함 동시성 제어를 위해 비관적 락 추가

--- src/main/java/net/causw/adapter/persistence/repository/LockerRepository.java
+++ src/main/java/net/causw/adapter/persistence/repository/LockerRepository.java
@@ -2,15 +2,21 @@ package net.causw.adapter.persistence.repository;
 
 import net.causw.adapter.persistence.locker.Locker;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Lock;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
+import javax.persistence.LockModeType;
 import java.util.List;
 import java.util.Optional;
 
 @Repository
 public interface LockerRepository extends JpaRepository<Locker, String> {
+
+    @Lock(LockModeType.PESSIMISTIC_WRITE)
+    Optional<Locker> findById(String id);
+
     Optional<Locker> findByLockerNumber(Long lockerNumber);
 
     Optional<Locker> findByUser_Id(String userId);
