f9d403aacce3943a4e2ecc1ddf633cafc2910d99
kitaee <dlrlxo999@naver.com>
2024-03-02 16:11:18 +0900

    feat: s3 이미지 연동 구현 완료

---
+++ src/main/java/net/causw/application/storage/StorageService.java
@@ -0,0 +1,42 @@
+package net.causw.application.storage;
+
+import com.amazonaws.services.s3.AmazonS3Client;
+import com.amazonaws.services.s3.model.CannedAccessControlList;
+import com.amazonaws.services.s3.model.ObjectMetadata;
+import com.amazonaws.services.s3.model.PutObjectRequest;
+import lombok.RequiredArgsConstructor;
+import net.causw.domain.model.util.S3Util;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Objects;
+
+@Service
+@RequiredArgsConstructor
+public class StorageService {
+
+    private final AmazonS3Client amazonS3Client;
+
+    @Value("${cloud.aws.s3.bucket}")
+    private String bucketName;
+
+    public String uploadFile(MultipartFile multipartFile) {
+
+        String fileName = S3Util.buildFileName(Objects.requireNonNull(multipartFile.getOriginalFilename()));
+
+        ObjectMetadata objectMetadata = new ObjectMetadata();
+        objectMetadata.setContentType(multipartFile.getContentType());
+
+        try (InputStream inputStream = multipartFile.getInputStream()) {
+            amazonS3Client.putObject(new PutObjectRequest(bucketName, fileName, inputStream, objectMetadata)
+                .withCannedAcl(CannedAccessControlList.PublicRead));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return amazonS3Client.getUrl(bucketName, fileName).toString().trim();
+    }
+}
