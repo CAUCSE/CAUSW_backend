dc05df0a4b3525cead2bb53db83e80aca76d5ff7
pstar987 <pstar987@naver.com>
2024-03-05 14:31:51 +0900

    hotfix : circle 공지 게시판 자동 생성 이름 변경

--- src/main/java/net/causw/application/circle/CircleService.java
+++ src/main/java/net/causw/application/circle/CircleService.java
@@ -297,11 +297,10 @@ public class CircleService {
 
         // Create boards of circle
         BoardDomainModel noticeBoard = BoardDomainModel.of(
-                newCircle.getName() + "공지 게시판",
+                newCircle.getName() + " 공지 게시판",
                 newCircle.getName() + " 공지 게시판",
                 Stream.of(Role.ADMIN, Role.PRESIDENT, Role.VICE_PRESIDENT, Role.LEADER_CIRCLE, Role.LEADER_1_N_LEADER_CIRCLE, Role.LEADER_2_N_LEADER_CIRCLE,
-                                Role.LEADER_3_N_LEADER_CIRCLE, Role.LEADER_4_N_LEADER_CIRCLE, Role.PRESIDENT_N_LEADER_CIRCLE,
-                                Role.COUNCIL_N_LEADER_CIRCLE, Role.VICE_PRESIDENT_N_LEADER_CIRCLE)
+                                Role.LEADER_3_N_LEADER_CIRCLE, Role.LEADER_4_N_LEADER_CIRCLE, Role.COUNCIL_N_LEADER_CIRCLE)
                         .map(Role::getValue)
                         .collect(Collectors.toList()),
                 "공지 게시판",
