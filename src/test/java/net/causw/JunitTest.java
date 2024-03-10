732b325c99ed342f5a63279a24b69412b5c32517
kitaee <dlrlxo999@naver.com>
2024-03-11 07:51:16 +0900

    feat: PR시, 테스트케이스 활성화

---
+++ src/test/java/net/causw/JunitTest.java
@@ -0,0 +1,17 @@
+package net.causw;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class JunitTest {
+
+    @Test
+    @DisplayName("Junit 테스트를 활성화합니다.")
+    void junitTest() {
+        String test = "동문 네트워크";
+        Assertions.assertEquals("동문 네트워크", test);
+    }
+}
