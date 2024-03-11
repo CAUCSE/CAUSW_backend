41799e8a3531dfbddefc3a69b70c24528ac9bd7d
kitaee <dlrlxo999@naver.com>
2024-03-11 20:24:33 +0900

    fix: profiles 파일을 dev가 아닌 test를 읽도록 수정

--- src/test/java/net/causw/adapter/web/BoardControllerTest.java
+++ src/test/java/net/causw/adapter/web/BoardControllerTest.java
@@ -9,7 +9,6 @@ import org.springframework.test.context.ActiveProfiles;
 import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
-@ActiveProfiles("dev")
 class BoardControllerTest {
 
     @Test
