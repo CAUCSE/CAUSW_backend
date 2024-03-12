190551500a59857cf52660d04ba11dbd49f6459a
kitaee <dlrlxo999@naver.com>
2024-03-12 18:08:34 +0900

    fix: profile 파일 BASE64로 인코딩 후 Secrets 변수에 등록

--- src/test/java/net/causw/JunitTest.java
+++ src/test/java/net/causw/JunitTest.java
@@ -4,9 +4,8 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.ActiveProfiles;
 
-@SpringBootTest(classes = JunitTest.class)
+@SpringBootTest
 public class JunitTest {
 
     @Test
--- src/test/java/net/causw/adapter/web/BoardControllerTest.java
+++ src/test/java/net/causw/adapter/web/BoardControllerTest.java
@@ -4,11 +4,10 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.ActiveProfiles;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-@SpringBootTest(classes = {BoardControllerTest.class})
+@SpringBootTest
 class BoardControllerTest {
 
     @Test
