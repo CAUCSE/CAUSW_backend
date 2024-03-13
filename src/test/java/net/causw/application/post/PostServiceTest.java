abcea5c5d1bbaf8842e608a5280307d087f14cb8
kitaee <dlrlxo999@naver.com>
2024-03-13 17:06:45 +0900

    add: 테스트케이스 추가

---
+++ src/test/java/net/causw/application/post/PostServiceTest.java
@@ -0,0 +1,19 @@
+package net.causw.application.post;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+class PostServiceTest {
+
+    @Test
+    @DisplayName("Junit 테스트를 활성화합니다.")
+    void junitTest() {
+        String test = "동문 네트워크";
+        Assertions.assertEquals("동문 네트워크", test);
+    }
+}
