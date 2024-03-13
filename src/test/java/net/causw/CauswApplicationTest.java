b466ff06146e425c0a08087d1e0b2ba4ac830277
kitaee <dlrlxo999@naver.com>
2024-03-13 17:02:22 +0900

    fix: SpringBootApplication 어노테이션을 붙여줌으로써 경로를 찾을 수 있도록 수정

--- src/test/java/net/causw/CauswApplicationTest.java
+++ src/test/java/net/causw/CauswApplicationTest.java
@@ -1,10 +1,11 @@
 package net.causw;
 
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-@SpringBootTest
+@SpringBootApplication
 class CauswApplicationTest {
 
 }
