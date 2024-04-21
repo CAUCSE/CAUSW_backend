3a7a2aef96890e61acbfa02ba8d4188ca525d792
CHAE <chaso1495@naver.com>
2024-04-21 17:02:33 +0900

    refactor: RequestDto 어노테이션 4종으로 통일

--- src/main/java/net/causw/application/dto/inquiry/InquiryCreateRequestDto.java
+++ src/main/java/net/causw/application/dto/inquiry/InquiryCreateRequestDto.java
@@ -2,11 +2,13 @@ package net.causw.application.dto.inquiry;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Getter
 @Setter
 @AllArgsConstructor
+@NoArgsConstructor
 public class InquiryCreateRequestDto {
     private String title;
     private String content;
