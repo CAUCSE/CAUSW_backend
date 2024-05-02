714a3ec6193cae29964c8796d4988eb462bf00a4
CHAE <chaso1495@naver.com>
2024-05-02 13:15:19 +0900

    refactor: CommentService Validator 중복 제거

--- src/main/java/net/causw/application/comment/CommentService.java
+++ src/main/java/net/causw/application/comment/CommentService.java
@@ -51,44 +51,14 @@ public class CommentService {
 
     @Transactional
     public CommentResponseDto createComment(String loginUserId, CommentCreateRequestDto commentCreateDto) {
-        ValidatorBucket validatorBucket = ValidatorBucket.of();
-
         User user = getUser(loginUserId);
         Post post = getPost(commentCreateDto.getPostId());
         Comment comment = Comment.of(commentCreateDto.getContent(), false, user, post);
 
-        validatorBucket
-                .consistOf(UserStateValidator.of(user.getState()))
-                .consistOf(UserRoleIsNoneValidator.of(user.getRole()))
-                .consistOf(TargetIsDeletedValidator.of(post.getBoard().getIsDeleted(), StaticValue.DOMAIN_BOARD))
-                .consistOf(TargetIsDeletedValidator.of(post.getIsDeleted(), StaticValue.DOMAIN_POST))
-                .consistOf(ConstraintValidator.of(comment, this.validator));
-
-        Optional<Circle> circles = Optional.ofNullable(post.getBoard().getCircle());
-        circles
-                .filter(circleDomainModel -> !user.getRole().equals(Role.ADMIN) && !user.getRole().getValue().contains("PRESIDENT"))
-                .ifPresent(
-                        circle -> {
-                            CircleMember member = circleMemberRepository.findByUser_IdAndCircle_Id(
-                                    loginUserId,
-                                    circle.getId()
-                            ).orElseThrow(
-                                    () -> new UnauthorizedException(
-                                            ErrorCode.NOT_MEMBER,
-                                            MessageUtil.CIRCLE_APPLY_INVALID
-                                    )
-                            );
-                            validatorBucket
-                                    .consistOf(TargetIsDeletedValidator.of(circle.getIsDeleted(), StaticValue.DOMAIN_CIRCLE))
-                                    .consistOf(CircleMemberStatusValidator.of(
-                                            member.getStatus(),
-                                            List.of(CircleMemberStatus.MEMBER)
-                                    ));
-                        }
-                );
-
-        validatorBucket
-                .validate();
+        ValidatorBucket validatorBucket = initializeValidator(user, post);
+        validatorBucket.
+                consistOf(ConstraintValidator.of(comment, this.validator));
+        validatorBucket.validate();
 
         return CommentResponseDto.of(
                 commentRepository.save(comment),
@@ -103,70 +73,29 @@ public class CommentService {
 
     @Transactional(readOnly = true)
     public Page<CommentResponseDto> findAllComments(String loginUserId, String postId, Integer pageNum) {
-        ValidatorBucket validatorBucket = ValidatorBucket.of();
-
         User user = getUser(loginUserId);
         Post post = getPost(postId);
 
-        validatorBucket
-                .consistOf(UserStateValidator.of(user.getState()))
-                .consistOf(UserRoleIsNoneValidator.of(user.getRole()))
-                .consistOf(TargetIsDeletedValidator.of(post.getBoard().getIsDeleted(), StaticValue.DOMAIN_BOARD))
-                .consistOf(TargetIsDeletedValidator.of(post.getIsDeleted(), StaticValue.DOMAIN_POST));
-
-        Optional<Circle> circles = Optional.ofNullable(post.getBoard().getCircle());
-        circles
-                .filter(circle -> !user.getRole().equals(Role.ADMIN) && !user.getRole().getValue().contains("PRESIDENT"))
-                .ifPresent(
-                        circle -> {
-                            CircleMember member = circleMemberRepository.findByUser_IdAndCircle_Id(
-                                    loginUserId,
-                                    circle.getId()
-                            ).orElseThrow(
-                                    () -> new UnauthorizedException(
-                                            ErrorCode.NOT_MEMBER,
-                                            MessageUtil.CIRCLE_APPLY_INVALID
-                                    )
-                            );
-
-                            validatorBucket
-                                    .consistOf(TargetIsDeletedValidator.of(circle.getIsDeleted(), StaticValue.DOMAIN_CIRCLE))
-                                    .consistOf(CircleMemberStatusValidator.of(
-                                            member.getStatus(),
-                                            List.of(CircleMemberStatus.MEMBER)
-                                    ));
-                        }
-                );
-
-        validatorBucket
-                .validate();
-
+        ValidatorBucket validatorBucket = initializeValidator(user, post);
+        validatorBucket.validate();
 
         Page<Comment> comments = commentRepository.findByPost_IdOrderByCreatedAt(
                 postId,
-                this.pageableFactory.create(pageNum, StaticValue.DEFAULT_COMMENT_PAGE_SIZE)
+                pageableFactory.create(pageNum, StaticValue.DEFAULT_COMMENT_PAGE_SIZE)
         );
-
-        for (Comment comment : comments) {
-            comment.setChildCommentList(childCommentRepository.findByParentComment_Id(comment.getId()));
-        }
+        comments.forEach(comment -> comment.setChildCommentList(childCommentRepository.findByParentComment_Id(comment.getId())));
 
         return comments.map(comment ->
-                        CommentResponseDto.of(
-                                comment,
-                                user,
-                                post.getBoard(),
-                                childCommentRepository.countByParentComment_IdAndIsDeletedIsFalse(comment.getId()),
-                                comment.getChildCommentList().stream()
-                                        .map(childComment -> ChildCommentResponseDto.of(
-                                                childComment,
-                                                user,
-                                                post.getBoard()
-                                        ))
-                                        .collect(Collectors.toList())
-                        )
-                );
-
+                CommentResponseDto.of(
+                        comment,
+                        user,
+                        post.getBoard(),
+                        childCommentRepository.countByParentComment_IdAndIsDeletedIsFalse(comment.getId()),
+                        comment.getChildCommentList().stream()
+                                .map(childComment -> ChildCommentResponseDto.of(childComment, user, post.getBoard()))
+                                .collect(Collectors.toList())
+                )
+        );
     }
 
     @Transactional
@@ -175,17 +104,12 @@ public class CommentService {
             String commentId,
             CommentUpdateRequestDto commentUpdateRequestDto
     ) {
-        ValidatorBucket validatorBucket = ValidatorBucket.of();
-
         User user = getUser(loginUserId);
         Comment comment = getComment(commentId);
         Post post = getPost(comment.getPost().getId());
 
+        ValidatorBucket validatorBucket = initializeValidator(user, post);
         validatorBucket
-                .consistOf(UserStateValidator.of(user.getState()))
-                .consistOf(UserRoleIsNoneValidator.of(user.getRole()))
-                .consistOf(TargetIsDeletedValidator.of(post.getBoard().getIsDeleted(), StaticValue.DOMAIN_BOARD))
-                .consistOf(TargetIsDeletedValidator.of(post.getIsDeleted(), StaticValue.DOMAIN_POST))
                 .consistOf(TargetIsDeletedValidator.of(comment.getIsDeleted(), StaticValue.DOMAIN_COMMENT))
                 .consistOf(ConstraintValidator.of(comment, this.validator))
                 .consistOf(ContentsAdminValidator.of(
@@ -194,25 +118,7 @@ public class CommentService {
                         comment.getWriter().getId(),
                         List.of()
                 ));
-
-        Optional<Circle> circles = Optional.ofNullable(post.getBoard().getCircle());
-        circles
-                .filter(circle -> !user.getRole().equals(Role.ADMIN) && !user.getRole().getValue().contains("PRESIDENT"))
-                .ifPresent(
-                        circle -> {
-                            CircleMember member = getCircleMember(loginUserId, circle.getId());
-
-                            validatorBucket
-                                    .consistOf(TargetIsDeletedValidator.of(circle.getIsDeleted(), StaticValue.DOMAIN_CIRCLE))
-                                    .consistOf(CircleMemberStatusValidator.of(
-                                            member.getStatus(),
-                                            List.of(CircleMemberStatus.MEMBER)
-                                    ));
-                        }
-                );
-
-        validatorBucket
-                .validate();
+        validatorBucket.validate();
 
         comment.update(commentUpdateRequestDto.getContent());
 
@@ -230,12 +136,11 @@ public class CommentService {
 
     @Transactional
     public CommentResponseDto deleteComment(String loginUserId, String commentId) {
-        ValidatorBucket validatorBucket = ValidatorBucket.of();
-
         User user = getUser(loginUserId);
         Comment comment = getComment(commentId);
         Post post = getPost(comment.getPost().getId());
 
+        ValidatorBucket validatorBucket = initializeValidator(user, post);
         validatorBucket
                 .consistOf(UserStateValidator.of(user.getState()))
                 .consistOf(UserRoleIsNoneValidator.of(user.getRole()))
@@ -246,15 +151,7 @@ public class CommentService {
                 .filter(circle -> !user.getRole().equals(Role.ADMIN) && !user.getRole().getValue().contains("PRESIDENT"))
                 .ifPresentOrElse(
                         circle -> {
-                            CircleMember member = circleMemberRepository.findByUser_IdAndCircle_Id(
-                                    loginUserId,
-                                    circle.getId()
-                            ).orElseThrow(
-                                    () -> new UnauthorizedException(
-                                            ErrorCode.NOT_MEMBER,
-                                            MessageUtil.CIRCLE_APPLY_INVALID
-                                    )
-                            );
+                            CircleMember member = getCircleMember(user.getId(), circle.getId());
 
                             validatorBucket
                                     .consistOf(TargetIsDeletedValidator.of(circle.getIsDeleted(), StaticValue.DOMAIN_CIRCLE))
@@ -291,9 +188,7 @@ public class CommentService {
                                         List.of()
                                 ))
                 );
-
-        validatorBucket
-                .validate();
+        validatorBucket.validate();
 
         comment.delete();
 
@@ -308,39 +203,63 @@ public class CommentService {
         );
     }
 
-    private User getUser(String userId){
+    private ValidatorBucket initializeValidator(User user, Post post) {
+        ValidatorBucket validatorBucket = ValidatorBucket.of();
+        validatorBucket
+                .consistOf(UserStateValidator.of(user.getState()))
+                .consistOf(UserRoleIsNoneValidator.of(user.getRole()))
+                .consistOf(TargetIsDeletedValidator.of(post.getBoard().getIsDeleted(), StaticValue.DOMAIN_BOARD))
+                .consistOf(TargetIsDeletedValidator.of(post.getIsDeleted(), StaticValue.DOMAIN_POST));
+
+        Optional<Circle> circles = Optional.ofNullable(post.getBoard().getCircle());
+        circles
+                .filter(circle -> !user.getRole().equals(Role.ADMIN) && !user.getRole().getValue().contains("PRESIDENT"))
+                .ifPresent(circle -> {
+                    CircleMember member = getCircleMember(user.getId(), circle.getId());
+
+                    validatorBucket
+                            .consistOf(TargetIsDeletedValidator.of(circle.getIsDeleted(), StaticValue.DOMAIN_CIRCLE))
+                            .consistOf(CircleMemberStatusValidator.of(
+                                    member.getStatus(),
+                                    List.of(CircleMemberStatus.MEMBER)
+                            ));
+                });
+        return validatorBucket;
+    }
+
+    private User getUser(String userId) {
         return userRepository.findById(userId).orElseThrow(
-            () -> new BadRequestException(
-                    ErrorCode.ROW_DOES_NOT_EXIST,
-                    MessageUtil.USER_NOT_FOUND
-            )
+                () -> new BadRequestException(
+                        ErrorCode.ROW_DOES_NOT_EXIST,
+                        MessageUtil.USER_NOT_FOUND
+                )
         );
     }
 
-    private Post getPost(String postId){
+    private Post getPost(String postId) {
         return postRepository.findById(postId).orElseThrow(
-            () -> new BadRequestException(
-                    ErrorCode.ROW_DOES_NOT_EXIST,
-                    MessageUtil.POST_NOT_FOUND
-            )
+                () -> new BadRequestException(
+                        ErrorCode.ROW_DOES_NOT_EXIST,
+                        MessageUtil.POST_NOT_FOUND
+                )
         );
     }
 
-    private Comment getComment(String commentId){
+    private Comment getComment(String commentId) {
         return commentRepository.findById(commentId).orElseThrow(
-            () -> new BadRequestException(
-                    ErrorCode.ROW_DOES_NOT_EXIST,
-                    MessageUtil.COMMENT_NOT_FOUND
-            )
+                () -> new BadRequestException(
+                        ErrorCode.ROW_DOES_NOT_EXIST,
+                        MessageUtil.COMMENT_NOT_FOUND
+                )
         );
     }
 
-    private CircleMember getCircleMember(String userId, String circleId){
+    private CircleMember getCircleMember(String userId, String circleId) {
         return circleMemberRepository.findByUser_IdAndCircle_Id(userId, circleId).orElseThrow(
-            () -> new BadRequestException(
-                    ErrorCode.ROW_DOES_NOT_EXIST,
-                    MessageUtil.CIRCLE_MEMBER_NOT_FOUND
-            )
+                () -> new UnauthorizedException(
+                        ErrorCode.NOT_MEMBER,
+                        MessageUtil.CIRCLE_APPLY_INVALID
+                )
         );
     }
 }
