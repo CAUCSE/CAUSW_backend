46f795c5a5b7903a75b2602cacf71eeb18b356bb
realisshomyang <realisshoman@kakao.com>
2024-05-26 23:49:59 +0900

    refactor : LockerActionEnable 재정의

--- src/main/java/net/causw/application/locker/LockerActionExtend.java
+++ src/main/java/net/causw/application/locker/LockerActionExtend.java
@@ -1,18 +1,15 @@
 package net.causw.application.locker;
 
 import lombok.NoArgsConstructor;
-import net.causw.application.spi.FlagPort;
-import net.causw.application.spi.LockerLogPort;
-import net.causw.application.spi.LockerPort;
-import net.causw.application.spi.TextFieldPort;
+import net.causw.adapter.persistence.locker.Locker;
+import net.causw.adapter.persistence.user.User;
+import net.causw.application.common.CommonService;
 import net.causw.domain.exceptions.BadRequestException;
 import net.causw.domain.exceptions.ErrorCode;
 import net.causw.domain.exceptions.InternalServerException;
-import net.causw.domain.model.locker.LockerDomainModel;
 import net.causw.domain.model.enums.Role;
 import net.causw.domain.model.util.MessageUtil;
 import net.causw.domain.model.util.StaticValue;
-import net.causw.domain.model.user.UserDomainModel;
 import net.causw.domain.validation.ExtendLockerExpiredAtValidator;
 import net.causw.domain.validation.UserRoleValidator;
 import net.causw.domain.validation.ValidatorBucket;
@@ -25,34 +22,32 @@ import java.util.Optional;
 @NoArgsConstructor
 public class LockerActionExtend implements LockerAction {
     @Override
-    public Optional<LockerDomainModel> updateLockerDomainModel(
-            LockerDomainModel lockerDomainModel,
-            UserDomainModel updaterDomainModel,
-            LockerPort lockerPort,
-            LockerLogPort lockerLogPort,
-            FlagPort flagPort,
-            TextFieldPort textFieldPort
+    public Optional<Locker> updateLockerDomainModel(
+            Locker locker,
+            User user,
+            LockerService lockerService,
+            CommonService commonService
     ) {
-        if (lockerDomainModel.getUser().isEmpty()) {
+        if (locker.getUser().isEmpty()) {
             throw new BadRequestException(
                     ErrorCode.CANNOT_PERFORMED,
                     MessageUtil.LOCKER_UNUSED
             );
         }
 
-        if (!updaterDomainModel.getId().equals(lockerDomainModel.getUser().get().getId()))
+        if (!user.getId().equals(locker.getUser().get().getId()))
             ValidatorBucket.of()
-                    .consistOf(UserRoleValidator.of(updaterDomainModel.getRole(), List.of(Role.PRESIDENT)))
+                    .consistOf(UserRoleValidator.of(user.getRole(), List.of(Role.PRESIDENT)))
                     .validate();
 
-        LocalDateTime expiredAtToExtend = LocalDateTime.parse(textFieldPort.findByKey(StaticValue.EXPIRED_AT).orElseThrow(
+        LocalDateTime expiredAtToExtend = LocalDateTime.parse(commonService.findByKeyInTextField(StaticValue.EXPIRED_AT).orElseThrow(
                 () -> new InternalServerException(
                         ErrorCode.INTERNAL_SERVER,
                         MessageUtil.LOCKER_RETURN_TIME_NOT_SET
                 )
         ), DateTimeFormatter.ofPattern("yyyy-MM-dd'T'HH:mm"));
 
-        Optional.ofNullable(lockerDomainModel.getExpiredAt()).ifPresent(expiredAt ->
+        Optional.ofNullable(locker.getExpireDate()).ifPresent(expiredAt ->
                 ValidatorBucket.of()
                         .consistOf(ExtendLockerExpiredAtValidator.of(
                                 expiredAt,
@@ -60,11 +55,8 @@ public class LockerActionExtend implements LockerAction {
                         .validate());
 
 
-        lockerDomainModel.extendExpireDate(expiredAtToExtend);
+        locker.extendExpireDate(expiredAtToExtend);
 
-        return lockerPort.update(
-                lockerDomainModel.getId(),
-                lockerDomainModel
-        );
+        return Optional.of(locker);
     }
 }
