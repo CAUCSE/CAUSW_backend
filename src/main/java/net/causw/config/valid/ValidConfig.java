b6620920492ebfdb2a1d984b8ef024b6450cb446
zestbear <zestbear90@gmail.com>
2024-08-05 22:22:44 +0900

    refactor : Proxy 설정 위한 configuration 추가

---
+++ src/main/java/net/causw/config/valid/ValidConfig.java
@@ -0,0 +1,9 @@
+package net.causw.config.valid;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
+
+@Configuration
+@EnableAspectJAutoProxy
+public class ValidConfig {
+}
