798996ef6f86f92a7b638d96caabd87d941441b9
pstar987 <pstar987@naver.com>
2024-08-13 18:43:29 +0900

    feat : formid 값이 securityservice에 전달되지 않는 문제가 발생하여 컨트롤러 메서드 실행 후 접근 권한 확인 기존 preauthorize와 마찬가지로 api의 접근 제한 관련이기 때문에 컨트롤러단에 위치시켰습니다.

--- src/main/java/net/causw/adapter/web/FormController.java
+++ src/main/java/net/causw/adapter/web/FormController.java
@@ -6,6 +6,9 @@ import net.causw.application.dto.form.FormResponseDto;
 import net.causw.application.form.FormService;
 import net.causw.config.security.SecurityService;
 import net.causw.config.security.userdetails.CustomUserDetails;
+import net.causw.domain.exceptions.ErrorCode;
+import net.causw.domain.exceptions.UnauthorizedException;
+import net.causw.domain.model.util.MessageUtil;
 import org.springframework.http.HttpStatus;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
@@ -16,6 +19,7 @@ import org.springframework.web.bind.annotation.*;
 @RequestMapping("/api/v1/forms")
 public class FormController {
     private final FormService formService;
+    private final SecurityService securityService;
 
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
@@ -29,8 +33,11 @@ public class FormController {
 
     @GetMapping("/{formId}")
     @ResponseStatus(HttpStatus.OK)
-    @PreAuthorize("@securityService.activeAndNotNoneUser and @securityService.hasAccessToForm(#formId)")
-    public FormResponseDto getForm(@PathVariable String formId) {
+    @PreAuthorize("@securityService.activeAndNotNoneUser")
+    public FormResponseDto getForm(@PathVariable(name = "formId") String formId) {
+        if (!securityService.hasAccessToForm(formId)) {
+            throw new UnauthorizedException(ErrorCode.API_NOT_ACCESSIBLE, MessageUtil.API_NOT_ACCESSIBLE);
+        }
         return formService.getForm(formId);
     }
 
