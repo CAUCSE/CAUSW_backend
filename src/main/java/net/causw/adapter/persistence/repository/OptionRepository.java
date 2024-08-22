f457ee4b85794a0483fec04fad95816969b2241a
pstar987 <pstar987@naver.com>
2024-08-22 21:50:17 +0900

    feat : question과 option의 repository 생성

---
+++ src/main/java/net/causw/adapter/persistence/repository/OptionRepository.java
@@ -0,0 +1,12 @@
+package net.causw.adapter.persistence.repository;
+
+import net.causw.adapter.persistence.form.Option;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface OptionRepository  extends JpaRepository<Option, String> {
+    Optional<Option> findById(String id);
+
+}
