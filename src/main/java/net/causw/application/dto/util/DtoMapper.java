23e78a3790551b44946661f9d9c7fb03bb1bb59c
pstar987 <pstar987@naver.com>
2024-08-24 01:14:03 +0900

    feat : 사용자별 결과반환을 위한 dto mapper

--- src/main/java/net/causw/application/dto/util/DtoMapper.java
+++ src/main/java/net/causw/application/dto/util/DtoMapper.java
@@ -3,6 +3,10 @@ package net.causw.application.dto.util;
 import net.causw.adapter.persistence.board.Board;
 import net.causw.adapter.persistence.comment.ChildComment;
 import net.causw.adapter.persistence.comment.Comment;
+import net.causw.adapter.persistence.form.Form;
+import net.causw.adapter.persistence.form.Option;
+import net.causw.adapter.persistence.form.Question;
+import net.causw.adapter.persistence.form.Reply;
 import net.causw.adapter.persistence.post.Post;
 import net.causw.adapter.persistence.user.User;
 import net.causw.application.dto.board.BoardMainResponseDto;
@@ -11,6 +15,7 @@ import net.causw.application.dto.board.BoardResponseDto;
 import net.causw.application.dto.comment.ChildCommentResponseDto;
 import net.causw.application.dto.comment.CommentResponseDto;
 import net.causw.application.dto.file.FileResponseDto;
+import net.causw.application.dto.form.*;
 import net.causw.application.dto.post.BoardPostsResponseDto;
 import net.causw.application.dto.post.PostContentDto;
 import net.causw.application.dto.post.PostResponseDto;
@@ -142,6 +147,18 @@ public interface DtoMapper{
 
 
     // Locker
+
+    // Form
+    FormResponseDto toFormResponseDto(Form form);
+
+    @Mapping(target = "questionId", source = "reply.question.id")
+    QuestionReplyResponseDto toQuestionReplyResponseDto(Reply reply);
+
+    // User 및 Reply 리스트 -> ReplyUserResponseDto로 매핑
+    @Mapping(target = "userId", source = "user.id")
+    @Mapping(target = "userName", source = "user.name")
+    @Mapping(target = "replies", source = "replies")
+    ReplyUserResponseDto toReplyUserResponseDto(User user, List<QuestionReplyResponseDto> replies);
 
 
 }
