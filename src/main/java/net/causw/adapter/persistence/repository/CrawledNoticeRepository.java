86b3ccaa2a2e1d1974fe50729af46278d43d2c19
givemethatsewon <nvpz1598@cau.ac.kr>
2024-09-01 01:04:49 +0900

    * feat : 웹 크롤러 완성

--- src/main/java/net/causw/adapter/persistence/repository/CrawledNoticeRepository.java
+++ src/main/java/net/causw/adapter/persistence/repository/CrawledNoticeRepository.java
@@ -4,5 +4,7 @@ package net.causw.adapter.persistence.repository;
 import net.causw.adapter.persistence.crawled.CrawledNotice;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface CrawledNoticeRepository extends JpaRepository<CrawledNotice, String> {
 }
