218de094a6cca530375d5d2907f8f06d9c96e30b
OH-GITAEK <dhrlxor1512@gmail.com>
2024-09-01 22:07:39 +0900

    refac : UserAdmissionResponseDto에 거부 사유필드값으로 추가 및 DtoMapper적용(#604)

--- src/main/java/net/causw/application/dto/user/UserAdmissionResponseDto.java
+++ src/main/java/net/causw/application/dto/user/UserAdmissionResponseDto.java
@@ -1,6 +1,7 @@
 package net.causw.application.dto.user;
 
 import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.persistence.Column;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.Setter;
@@ -32,29 +33,6 @@ public class UserAdmissionResponseDto {
     @Schema(description = "마지막 업데이트된 시각", example = "2024-01-24T00:26:40.643Z")
     private LocalDateTime updatedAt;
 
-
-    public static UserAdmissionResponseDto from(UserAdmission userAdmission) {
-        return UserAdmissionResponseDto.builder()
-                .id(userAdmission.getId())
-                .user(UserResponseDto.from(userAdmission.getUser()))
-                .attachImage(userAdmission.getAttachImage())
-                .description(userAdmission.getDescription())
-                .createdAt(userAdmission.getCreatedAt())
-                .updatedAt(userAdmission.getUpdatedAt())
-                .build();
-    }
-
-    public static UserAdmissionResponseDto of(
-            UserAdmission userAdmission,
-            User user
-    ) {
-        return UserAdmissionResponseDto.builder()
-                .id(userAdmission.getId())
-                .user(UserResponseDto.from(user))
-                .attachImage(userAdmission.getAttachImage())
-                .description(userAdmission.getDescription())
-                .createdAt(userAdmission.getCreatedAt())
-                .updatedAt(userAdmission.getUpdatedAt())
-                .build();
-    }
+    @Schema(description = "거부 사유", example = "양식과 다른 사진을 제출하여 거절되었습니다")
+    private String rejectReason;
 }
