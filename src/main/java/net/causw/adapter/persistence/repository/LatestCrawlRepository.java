514a3c11a5352b7fb1752b78718a50eaf9a10d08
givemethatsewon <nvpz1598@cau.ac.kr>
2024-09-03 06:21:48 +0900

    * fix : LatestCrawlRepository.java repository 계정 @Transactional 어노테이션 삭제

--- src/main/java/net/causw/adapter/persistence/repository/LatestCrawlRepository.java
+++ src/main/java/net/causw/adapter/persistence/repository/LatestCrawlRepository.java
@@ -1,7 +1,6 @@
 package net.causw.adapter.persistence.repository;
 
 import jakarta.transaction.Transactional;
-import jdk.jfr.Category;
 import net.causw.adapter.persistence.crawled.LatestCrawl;
 import net.causw.domain.model.enums.CrawlCategory;
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -15,7 +14,6 @@ public interface LatestCrawlRepository extends JpaRepository<LatestCrawl, String
     Optional<LatestCrawl> findByCrawlCategory(CrawlCategory crawlCategory);
 
     @Modifying
-    @Transactional
     @Query("UPDATE LatestCrawl lc SET lc.latestUrl = :newUrl WHERE lc.crawlCategory = :crawlCategory")
     void updateLatestUrlByCategory(@Param("newUrl") String newUrl, @Param("crawlCategory") CrawlCategory crawlCategory);
 
