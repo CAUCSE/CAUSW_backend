ed6b29cf844749b5822b993dda3709e27a8bef33
zestbear <zestbear90@gmail.com>
2024-09-08 07:12:42 +0900

    refac : 사용자 관련 Request Dto에 Valid 적용

--- src/main/java/net/causw/application/dto/user/UserAdmissionCreateRequestDto.java
+++ src/main/java/net/causw/application/dto/user/UserAdmissionCreateRequestDto.java
@@ -1,6 +1,8 @@
 package net.causw.application.dto.user;
 
 import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -15,6 +17,8 @@ import java.util.Optional;
 @NoArgsConstructor
 public class UserAdmissionCreateRequestDto {
 
+    @Email(message = "이메일 형식에 맞지 않습니다.")
+    @NotBlank(message = "이메일을 입력해 주세요.")
     @Schema(description = "이메일", example = "yebin@cau.ac.kr")
     private String email;
 
