1245817f5b911b9299f5d7d3a04b8c8f936ee3c3
acidify04 <peh4622@naver.com>
2024-09-07 22:09:26 +0900

    feat: BoardApplyRepository에 findAll 메서드 추가

--- src/main/java/net/causw/adapter/persistence/repository/BoardApplyRepository.java
+++ src/main/java/net/causw/adapter/persistence/repository/BoardApplyRepository.java
@@ -4,6 +4,9 @@ import net.causw.adapter.persistence.board.BoardApply;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface BoardApplyRepository extends JpaRepository<BoardApply, String> {
+    List<BoardApply> findAll();
 }
