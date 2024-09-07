0dc2f472639faddf6a9351cc2d3218489a9ddd46
zestbear <zestbear90@gmail.com>
2024-09-08 07:12:30 +0900

    refac : 설문지/신청서 관련 Request Dto에 Valid 적용

--- src/main/java/net/causw/application/dto/form/QuestionCreateRequestDto.java
+++ src/main/java/net/causw/application/dto/form/QuestionCreateRequestDto.java
@@ -2,6 +2,8 @@ package net.causw.application.dto.form;
 
 
 import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import lombok.*;
 
 import java.util.List;
@@ -12,12 +14,15 @@ import java.util.List;
 @AllArgsConstructor
 @NoArgsConstructor
 public class QuestionCreateRequestDto {
+    @NotNull(message = "질문 번호를 입력해 주세요.")
     @Schema(description = "질문 번호", example = "1")
     private Integer questionNumber;
 
+    @NotBlank(message = "질문 내용을 입력해 주세요.")
     @Schema(description = "질문 내용", example = "1번 문제입니다.")
     private String questionText;
 
+    @NotNull(message = "복수 정답 여부를 선택해 주세요.")
     @Schema(description = "복수 정답 여부(객관식일 때만)", defaultValue = "false")
     private Boolean isMultiple;
 
