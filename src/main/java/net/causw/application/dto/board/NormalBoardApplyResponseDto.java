933d5ec1a3152a297ab4ce189cb82935d18cb6c1
acidify04 <peh4622@naver.com>
2024-09-07 21:41:22 +0900

    feat: NormalBoardApplyResponseDto class 생성 및 세팅

---
+++ src/main/java/net/causw/application/dto/board/NormalBoardApplyResponseDto.java
@@ -0,0 +1,13 @@
+package net.causw.application.dto.board;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class NormalBoardApplyResponseDto {
+}
