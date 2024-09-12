467a4d8c803b806a62992000afe9452484d8119a
acidify04 <peh4622@naver.com>
2024-09-12 09:46:07 +0900

    refactor: 불필요한 enum value값 삭제

--- src/main/java/net/causw/domain/model/enums/BoardApplyStatus.java
+++ src/main/java/net/causw/domain/model/enums/BoardApplyStatus.java
@@ -2,13 +2,8 @@ package net.causw.domain.model.enums;
 
 
 public enum BoardApplyStatus {
-    AWAIT("AWAIT"),
-    ACCEPTED("ACCEPTED"),
-    REJECT("REJECT");
+    AWAIT,
+    ACCEPTED,
+    REJECT;
 
-    private final String value;
-
-    BoardApplyStatus(String value) {
-        this.value = value;
-    }
 }
