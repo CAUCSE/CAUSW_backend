a8270d33cb908dc7d51f416cddfe32d8ba092088
zestbear <zestbear90@gmail.com>
2024-09-23 14:15:10 +0900

    feature : 주관식/객관식 타입 추가

--- src/main/java/net/causw/adapter/persistence/form/Question.java
+++ src/main/java/net/causw/adapter/persistence/form/Question.java
@@ -3,6 +3,7 @@ package net.causw.adapter.persistence.form;
 import jakarta.persistence.*;
 import lombok.*;
 import net.causw.adapter.persistence.base.BaseEntity;
+import net.causw.domain.model.enums.QuestionType;
 import org.hibernate.annotations.ColumnDefault;
 
 import java.util.List;
@@ -18,6 +19,10 @@ public class Question extends BaseEntity {
     @Column(name = "number", nullable = false)
     private Integer number;
 
+    @Column(name = "question_type", nullable = false)
+    @Enumerated(EnumType.STRING)
+    private QuestionType questionType;
+
     @Column(name = "question_text", nullable = false)
     private String questionText;
 
@@ -32,18 +37,15 @@ public class Question extends BaseEntity {
     @JoinColumn(name = "form_id", nullable = false)
     private Form form;
 
-    public static Question of(Integer number, String questionText, Boolean isMultiple, List<Option> options, Form form) {
+    public static Question of(Integer number, QuestionType questionType, String questionText, Boolean isMultiple, List<Option> options, Form form) {
         return Question.builder()
                 .number(number)
+                .questionType(questionType)
                 .questionText(questionText)
                 .isMultiple(isMultiple)
                 .options(options)
                 .form(form)
                 .build();
-    }
-
-    public void setIsMultiple(Boolean isMultiple) {
-        this.isMultiple = isMultiple;
     }
 
 
---
+++ src/main/java/net/causw/domain/model/enums/QuestionType.java
@@ -0,0 +1,6 @@
+package net.causw.domain.model.enums;
+
+public enum QuestionType {
+    SUBJECTIVE, // 주관식
+    OBJECTIVE // 객관식
+}
