8de5abbd936ae413a00f07c259cee41b8a999bed
realisshomyang <realisshoman@kakao.com>
2025-02-26 21:01:12 +0900

    feat : NotificationService 선언(댓글, 게시글, 경조사)

---
+++ src/main/java/net/causw/adapter/persistence/repository/notification/CeremonyNotificationSettingRepository.java
@@ -0,0 +1,19 @@
+package net.causw.adapter.persistence.repository.notification;
+
+import net.causw.adapter.persistence.notification.CeremonyNotificationSetting;
+import net.causw.adapter.persistence.user.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Optional;
+
+@Repository
+public interface CeremonyNotificationSettingRepository extends JpaRepository<CeremonyNotificationSetting, String> {
+    Optional<CeremonyNotificationSetting> findByUser(User user);
+    @Query("SELECT c FROM CeremonyNotificationSetting c " +
+            "WHERE :admissionYear MEMBER OF c.subscribedAdmissionYears OR c.isSetAll = true")
+    List<CeremonyNotificationSetting> findByAdmissionYearOrSetAll(@Param("admissionYear") Integer admissionYear);
+}
