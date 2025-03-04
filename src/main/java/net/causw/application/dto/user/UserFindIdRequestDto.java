d3524ccc37806fa39a5cfdd5e00dfb769a391763
OH-GITAEK <dhrlxor1512@gmail.com>
2025-03-04 22:30:02 +0900

    fix : 전화번호 양식 전체 통일

--- src/main/java/net/causw/application/dto/user/UserFindIdRequestDto.java
+++ src/main/java/net/causw/application/dto/user/UserFindIdRequestDto.java
@@ -6,18 +6,17 @@ import lombok.Getter;
 
 @Getter
 public class UserFindIdRequestDto {
-    // TODO : private으로 안한 특별한 이유가 있는가?
     @NotBlank(message = "학번은 비어있을 수 없습니다.")
-    String studentId;
+    private String studentId;
 
     @NotBlank(message = "이름은 비어있을 수 없습니다.")
-    String name;
+    private String name;
 
     @Pattern(
             regexp = "^01(?:0|1|[6-9])-(\\d{3}|\\d{4})-\\d{4}$",
             message = "전화번호 형식에 맞지 않습니다."
     )
     @NotBlank(message = "전화번호는 비어있을 수 없습니다.")
-    String phoneNumber;
+    private String phoneNumber;
 
 }
