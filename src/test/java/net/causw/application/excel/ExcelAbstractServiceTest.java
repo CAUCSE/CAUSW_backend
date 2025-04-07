b4f9c2d164bf89ecccbf4462de90cacf1f9da54a
GY102912 <qaz102912@gmail.com>
2025-04-07 16:01:23 +0900

    test: usingRecursiveAssertion 수정 및 테스트 내용 추가

--- src/test/java/net/causw/application/excel/ExcelAbstractServiceTest.java
+++ src/test/java/net/causw/application/excel/ExcelAbstractServiceTest.java
@@ -2,33 +2,28 @@ package net.causw.application.excel;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
 
 import java.io.ByteArrayInputStream;
 import java.io.IOException;
-import java.time.LocalDateTime;
 import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.stream.Stream;
-import net.causw.adapter.persistence.user.User;
-import net.causw.adapter.persistence.userCouncilFee.UserCouncilFee;
 import net.causw.application.dto.user.UserResponseDto;
 import net.causw.application.dto.userCouncilFee.UserCouncilFeeResponseDto;
-import net.causw.application.dto.util.dtoMapper.UserCouncilFeeDtoMapper;
-import net.causw.application.dto.util.dtoMapper.UserDtoMapper;
 import net.causw.domain.exceptions.InternalServerException;
 import org.apache.poi.ss.formula.functions.T;
+import org.apache.poi.ss.usermodel.CellType;
 import org.apache.poi.ss.usermodel.Row;
 import org.apache.poi.ss.usermodel.Sheet;
 import org.apache.poi.ss.usermodel.Workbook;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.extension.ExtendWith;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
-import org.mockito.InjectMocks;
-import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.mock.web.MockHttpServletResponse;
 
 public class ExcelAbstractServiceTest {
@@ -49,6 +44,7 @@ public class ExcelAbstractServiceTest {
   void testGenerateExcelSuccess(
       ExcelAbstractService<T> service, List<T> dataList) throws IOException {
     // given
+    int expectedRowNum = dataList.size() + 1, expectedColNum = headerStringList.size();
     MockHttpServletResponse response = new MockHttpServletResponse();
     LinkedHashMap<String, List<T>> sheetDataMap = new LinkedHashMap<>();
     sheetDataMap.put(sheetName, dataList);
@@ -74,6 +70,10 @@ public class ExcelAbstractServiceTest {
     assertThat(createdSheet.getSheetName())
         .as("시트 이름이 sheetDataMap에 포함되어야 합니다.")
         .isIn(sheetDataMap.keySet());
+
+    verifySheet(createdSheet, expectedRowNum);
+    verifyHeaderRow(createdSheet.getRow(0), expectedColNum);
+    verifyDataRow(createdSheet.getRow(1));
   }
 
   @ParameterizedTest
@@ -124,8 +124,9 @@ public class ExcelAbstractServiceTest {
     // then
     Sheet createdSheet = workbook.getSheetAt(0);
 
-    verifyRow(createdSheet,expectedRowNum);
-    verifyCell(createdSheet, expectedColNum);
+    verifySheet(createdSheet, expectedRowNum);
+    verifyHeaderRow(createdSheet.getRow(0), expectedColNum);
+    verifyDataRow(createdSheet.getRow(1));
   }
 
   @ParameterizedTest
@@ -143,24 +144,56 @@ public class ExcelAbstractServiceTest {
     // then
     Sheet createdSheet = workbook.getSheetAt(0);
 
-    verifyRow(createdSheet,expectedRowNum);
-    verifyCell(createdSheet, expectedColNum);
+    verifySheet(createdSheet, expectedRowNum);
+    verifyHeaderRow(createdSheet.getRow(0), expectedColNum);
   }
 
-  private void verifyRow(Sheet sheet, int expectedRowNum) {
+  private void verifySheet(Sheet sheet, int expectedRowNum) {
+    assertThat(sheet).isNotNull();
     assertThat(sheet.getPhysicalNumberOfRows())
-        .as("헤더를 포함한 행의 개수는 %s이어야 합니다.", expectedRowNum)
+        .as("헤더를 포함한 행의 개수가 예측값과 같아야 합니다.")
         .isEqualTo(expectedRowNum);
+
+    sheet.iterator().forEachRemaining(
+        row -> assertThat(row)
+              .as("모든 행은 null이 아니어야 합니다.")
+              .isNotNull()
+    );
   }
 
-  private void verifyCell(Sheet sheet, int expectedColNum) {
-    Row header = sheet.getRow(0);
-
-    assertThat(header.getPhysicalNumberOfCells())
-        .as("열의 개수는 headerStringList의 크기와 같아야 합니다.")
+  private void verifyHeaderRow(Row row, int expectedColNum) {
+    assertThat(row).isNotNull();
+    assertThat(row.getPhysicalNumberOfCells())
+        .as("셀의 개수가 예측값과 같아야 합니다.")
         .isEqualTo(expectedColNum);
-    assertThat(sheet)
-        .as("모든 셀은 null이 아니어야 합니다.")
-        .usingRecursiveAssertion().isNotNull();
+
+    row.iterator().forEachRemaining(
+        cell -> {
+          assertThat(cell)
+              .as("모든 셀은 null이 아니어야 합니다.")
+              .isNotNull();
+          assertThat(cell.getCellType())
+              .as("모든 셀은 STRING 타입이어야 합니다.")
+              .isNotNull().isEqualTo(CellType.STRING);
+          assertThat(cell.getStringCellValue())
+              .as("실제 엑셀의 헤더와 headerStringList의 내용이 일치해야 합니다.")
+              .isNotNull().isEqualTo(headerStringList.get(cell.getColumnIndex()));
+        }
+    );
+  }
+
+  private void verifyDataRow(Row row) {
+    assertThat(row).isNotNull();
+
+    row.iterator().forEachRemaining(
+        cell -> {
+          assertThat(cell)
+              .as("모든 셀은 null이 아니어야 합니다.")
+              .isNotNull();
+          assertThat(cell.getCellType())
+              .as("모든 셀은 STRING 타입이어야 합니다.")
+              .isNotNull().isEqualTo(CellType.STRING);
+        }
+    );
   }
 }
