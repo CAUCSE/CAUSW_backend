9e2a894f73c3551569018403c8734cfa69e26c8d
bingle625 <fhqhxm8899@gmail.com>
2025-04-08 00:12:21 +0900

    refactor: 게시글 좋아요 관련 공통 유효성 검사 메서드 분리

--- src/main/java/net/causw/application/post/PostService.java
+++ src/main/java/net/causw/application/post/PostService.java
@@ -641,14 +641,16 @@ public class PostService {
         post.setIsDeleted(false);
     }
 
+    /**
+     * 게시글 좋아요 메서드
+     * @param user 좋아요 누른 유저
+     * @param postId 좋아요 누른 게시글 아이디
+     */
     @Transactional
     public void likePost(User user, String postId) {
         Post post = getPost(postId);
 
-        ValidatorBucket validatorBucket = ValidatorBucket.of();
-        validatorBucket
-                .consistOf(UserStateIsDeletedValidator.of(post.getWriter().getState()))
-                .validate();
+        validateWriterNotDeleted(post);
 
         if (isPostLiked(user, postId)) {
             throw new BadRequestException(ErrorCode.ROW_ALREADY_EXIST, MessageUtil.POST_ALREADY_LIKED);
@@ -661,16 +663,13 @@ public class PostService {
     /**
      * 게시글 좋아요 취소 메서드
      * @param user 좋아요 취소 누른 유저
-     * @param postId 좋아요 취소 누른 게시글
+     * @param postId 좋아요 취소 누른 게시글 아이디
      */
     @Transactional
     public void cancelLikePost(final User user, final String postId) {
         Post post = getPost(postId);
 
-        ValidatorBucket validatorBucket = ValidatorBucket.of();
-        validatorBucket
-            .consistOf(UserStateIsDeletedValidator.of(post.getWriter().getState()))
-            .validate();
+        this.validateWriterNotDeleted(post);
 
         if (!isPostLiked(user, postId)) {
             throw new BadRequestException(ErrorCode.ROW_DOES_NOT_EXIST, MessageUtil.POST_NOT_LIKED);
@@ -1167,5 +1166,16 @@ public class PostService {
                 .map(voteRecord -> UserDtoMapper.INSTANCE.toUserResponseDto(voteRecord.getUser(), null, null))
                 .collect(Collectors.toList());
         return VoteDtoMapper.INSTANCE.toVoteOptionResponseDto(voteOption, voteRecords.size(), userResponseDtos);
+    }
+
+    /**
+     * 게시글의 글쓴이가 삭제된 사용자인지 유효성 검사
+     * @param post 게시글
+     */
+    private void validateWriterNotDeleted(final Post post) {
+        ValidatorBucket validatorBucket = ValidatorBucket.of();
+        validatorBucket
+            .consistOf(UserStateIsDeletedValidator.of(post.getWriter().getState()))
+            .validate();
     }
 }
