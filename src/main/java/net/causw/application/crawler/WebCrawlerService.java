bb76087490468b8a7c48c821918b69d8c0ff2329
hanjk0816 <hanjk0816@naver.com>
2025-04-15 12:25:51 +0900

    fix: content저장 방식 수정, 메서드 분리 완료

--- src/main/java/net/causw/application/crawler/WebCrawlerService.java
+++ src/main/java/net/causw/application/crawler/WebCrawlerService.java
@@ -37,32 +37,25 @@ public class WebCrawlerService {
     @Scheduled(cron = "0 0 * * * *") // 매 시각 0분 0초에 실행 (배포용)
     @Transactional
     public void crawlAndSaveCAUSWNoticeSite()  {
-        int pageNum = 1;
-
         // 최신 URL 가져오기
         String recentNoticeLink = latestCrawlRepository.findByCrawlCategory(CrawlCategory.CAU_SW_NOTICE)
                 .map(LatestCrawl::getLatestUrl)
                 .orElse(null);
 
+        int pageNum = 1;
         boolean isNew = true;
         while (isNew) {
             String url = StaticValue.CAU_CSE_BASE_URL + pageNum;
-            Document doc;
-            try {
-                doc = Jsoup.connect(url).get();
-            } catch (IOException e) {
-                throw new InternalServerException(ErrorCode.INTERNAL_SERVER, MessageUtil.FAIL_TO_CRAWL_CAU_SW_NOTICE_SITE);
-            }
+            Document doc = fetchUrl(url);
 
             Elements rows = doc.select("table.table-basic tbody tr");
-
             if (rows.isEmpty()) {
                 break; // 더 이상 페이지가 없으면 종료
             }
 
             List<CrawledNotice> notices = new ArrayList<>();
+
             for (Element row : rows) {
-                String noticeType = row.select("td span.tag").text();
                 Element titleElement = row.select("td.aleft a").first();
                 if (titleElement == null) {
                     continue;
@@ -74,60 +67,10 @@ public class WebCrawlerService {
                     isNew = false;
                     break;
                 }
-                // 상세 페이지로 이동하여 내용 크롤링
-                Document detailDoc;
-                try {
-                    detailDoc = Jsoup.connect(absoluteLink).get();
-                } catch (IOException e) {
-                    throw new InternalServerException(ErrorCode.INTERNAL_SERVER, MessageUtil.FAIL_TO_CRAWL_CAU_SW_NOTICE_SITE);
-                }
-                String title = detailDoc.select("div.header > h3").text();  // 제목 추출
-                String announceDate = detailDoc.select("div.header > div > span").get(1).text();    // 작성일 추출
-                String author = detailDoc.select("div.header > div > span").get(3).text();  // 작성자 추출
-                String content = detailDoc.select("div.fr-view").outerHtml();   // 본문 내용 추출
-                String imageLink = detailDoc.select("div.fr-view > p > img").attr("abs:src");  // 절대경로로 이미지 추출 => 없는 경우 빈 문자열 삽입\
-
-
-                // 첨부파일 다운로드 경로 추출
-                Elements downloadLinks = detailDoc.select("div.files span");
-                List<CrawledFileLink> crawledFileLinks = new ArrayList<>();
-
-                for (Element link : downloadLinks) {
-                    // 정규식 사용
-                    String onclickAttr = link.attr("onclick");
-                    Pattern pattern = Pattern.compile("goLocation\\('/_module/bbs/download.php','(\\d+)','(\\w+)'\\).*?>(.*?)<");
-                    Matcher matcher = pattern.matcher(link.outerHtml());
-
-                    while (matcher.find()) {
-                        String uid = matcher.group(1); // uid 추출
-                        String code = matcher.group(2); // code 추출
-                        String fileName = matcher.group(3).trim(); // 파일명 추출
-                        // 파일 다운로드 경로 생성
-                        String fileUrl = String.format("https://cse.cau.ac.kr/_module/bbs/download.php?uid=%s&code=%s", uid, code);
-                        // CrawledFileLink 객체 생성
-                        crawledFileLinks.add(CrawledFileLink.of(fileName, fileUrl));
-                    }
-                }
-
-                if (downloadLinks.isEmpty()) {
-                    // 첨부파일이 없는 경우
-                    crawledFileLinks = null;
-                }
-
-                // CrawledNotice 객체 생성
-                CrawledNotice notice = CrawledNotice.of(
-                        noticeType,
-                        title,
-                        content,
-                        absoluteLink,
-                        author,
-                        announceDate,
-                        imageLink,
-                        crawledFileLinks
-                );
+                CrawledNotice notice = parseNotice(row, absoluteLink);
                 notices.add(notice);
-
             }
+
             // DB save
             if (!notices.isEmpty()) {
                 // 새로운 공지사항이 하나라도 있으면 저장
@@ -151,5 +94,82 @@ public class WebCrawlerService {
             pageNum++;
         }
     }
-}
 
+    //HTML페이지를 가져옴
+    private Document fetchUrl(String url) {
+        try {
+            return Jsoup.connect(url).get();
+        } catch (IOException e) {
+            throw new InternalServerException(ErrorCode.INTERNAL_SERVER, MessageUtil.FAIL_TO_CRAWL_CAU_SW_NOTICE_SITE);
+        }
+    }
+
+    // 상세 페이지로 이동하여 내용 크롤링
+    private CrawledNotice parseNotice(Element row, String noticeUrl) {
+        String noticeType = row.select("td span.tag").text();
+        Document detailDoc = fetchUrl(noticeUrl);
+
+        String title = detailDoc.select("div.header > h3").text();  // 제목 추출
+        String announceDate = detailDoc.select("div.header > div > span").get(1).text();    // 작성일 추출
+        String author = detailDoc.select("div.header > div > span").get(3).text();  // 작성자 추출
+
+        String imageLink = detailDoc.select("div.fr-view > p > img").attr("abs:src");  // 절대경로로 이미지 추출 => 없는 경우 빈 문자열 삽입
+
+        // 본문 내용 추출
+        Element contentElement = detailDoc.select("div.fr-view").first();
+        StringBuilder textWithLineBreaks = new StringBuilder();
+
+        // 태그 기준으로 줄바꿈
+        for (Element elem : contentElement.getAllElements()) {
+            switch (elem.tagName()) {
+                case "p":
+                case "li":
+                    textWithLineBreaks.append(elem.text()).append("\n");
+                    break;
+                case "br":
+                    textWithLineBreaks.append("\n");
+                    break;
+            }
+        }
+        String content = textWithLineBreaks.toString().trim();
+
+        List<CrawledFileLink> fileLinks = extractDownloadLink(detailDoc);
+        return CrawledNotice.of(
+                noticeType,
+                title,
+                content,
+                noticeUrl,
+                author,
+                announceDate,
+                imageLink,
+                fileLinks.isEmpty() ? null : fileLinks
+        );
+    }
+
+    //첨부파일 다운로드 정보를 찾고 다운로드 가능한 파일링크 생성
+    private List<CrawledFileLink> extractDownloadLink(Document detailDoc) {
+        List<CrawledFileLink> crawledFileLinks = new ArrayList<>();
+        Elements downloadElements = detailDoc.select("div.files span");
+
+        // 첨부파일 다운로드 경로 추출
+        Elements downloadLinks = detailDoc.select("div.files span");
+        Pattern pattern = Pattern.compile("goLocation\\('/_module/bbs/download.php','(\\d+)','(\\w+)'\\).*?>(.*?)<");
+
+        for (Element link : downloadLinks) {
+            // 정규식 사용
+            String onclickAttr = link.attr("onclick");
+            Matcher matcher = pattern.matcher(link.outerHtml());
+
+            while (matcher.find()) {
+                String uid = matcher.group(1); // uid 추출
+                String code = matcher.group(2); // code 추출
+                String fileName = matcher.group(3).trim(); // 파일명 추출
+                // 파일 다운로드 경로 생성
+                String fileUrl = String.format("https://cse.cau.ac.kr/_module/bbs/download.php?uid=%s&code=%s", uid, code);
+                // CrawledFileLink 객체 생성
+                crawledFileLinks.add(CrawledFileLink.of(fileName, fileUrl));
+            }
+        }
+        return crawledFileLinks;
+    }
+}
