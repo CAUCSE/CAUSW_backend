4e1cf93a9bdb490c489a8c0387367b164801d5c8
idle2534 <idle2534@outlook.com>
2025-05-29 23:01:14 +0900

    feat: 공통된 권한 검사를 WebSecurityConfig.java 에서 관리하도록 변경 - SecurityEndpoints.java에 순서 고려없이 추가할 수 있도록 포함관계 기준으로 적용하는 기능 추가

---
+++ src/main/java/net/causw/config/security/CustomAuthorizationManager.java
@@ -0,0 +1,36 @@
+package net.causw.config.security;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.authorization.AuthenticatedAuthorizationManager;
+import org.springframework.security.authorization.AuthorizationDecision;
+import org.springframework.security.authorization.AuthorizationManager;
+import org.springframework.security.web.access.intercept.RequestAuthorizationContext;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
+public class CustomAuthorizationManager {
+    SecurityService securityService;
+
+    public AuthorizationManager<RequestAuthorizationContext> isActiveAndNotNoneUserAndAcademicRecordCertified() {
+        return (authentication, context) -> {
+            boolean isAuthenticated = securityService.isActiveAndNotNoneUserAndAcademicRecordCertified();
+            return new AuthorizationDecision(isAuthenticated);
+        };
+    }
+
+    public AuthorizationManager<RequestAuthorizationContext> isActiveAndNotNoneUser() {
+        return (authentication, context) -> {
+            boolean isAuthenticated = securityService.isActiveAndNotNoneUser();
+            return new AuthorizationDecision(isAuthenticated);
+        };
+    }
+
+    public AuthorizationManager<RequestAuthorizationContext> permitAll() {
+        return (authentication, context) -> new AuthorizationDecision(true);
+    }
+
+    public AuthorizationManager<RequestAuthorizationContext> authenticated() {
+        return AuthenticatedAuthorizationManager.authenticated();
+    }
+}
