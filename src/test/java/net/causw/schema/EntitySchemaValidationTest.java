2743c9cd3e538f22d2df4f375cee27e2b20dc708
GY102912 <qaz102912@gmail.com>
2025-06-12 01:45:52 +0900

    data jpa test로 변경

--- src/test/java/net/causw/schema/EntitySchemaValidationTest.java
+++ src/test/java/net/causw/schema/EntitySchemaValidationTest.java
@@ -1,11 +1,11 @@
 package net.causw.schema;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.test.context.ActiveProfiles;
 
 @ActiveProfiles("test")
-@SpringBootTest
+@DataJpaTest
 public class EntitySchemaValidationTest {
 
   @Test
