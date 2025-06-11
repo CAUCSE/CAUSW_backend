2687cc06f1a4a3714ede5d35239c1262a1eb84d5
GY102912 <qaz102912@gmail.com>
2025-06-11 19:50:39 +0900

    프로필 인식 설정

--- src/test/java/net/causw/schema/EntitySchemaValidationTest.java
+++ src/test/java/net/causw/schema/EntitySchemaValidationTest.java
@@ -2,7 +2,9 @@ package net.causw.schema;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
+@ActiveProfiles("test")
 @SpringBootTest
 public class EntitySchemaValidationTest {
 
