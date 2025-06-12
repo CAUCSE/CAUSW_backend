3766e1f25c10cd9ec648bc9e713d9a81a194c67f
GY102912 <qaz102912@gmail.com>
2025-06-12 15:23:27 +0900

    Test 수정

--- src/test/java/net/causw/schema/EntitySchemaValidationTest.java
+++ src/test/java/net/causw/schema/EntitySchemaValidationTest.java
@@ -1,69 +1,15 @@
 package net.causw.schema;
 
-import jakarta.persistence.EntityManagerFactory;
-import java.util.HashMap;
-import java.util.Map;
-import org.hibernate.boot.Metadata;
-import org.hibernate.boot.MetadataSources;
-import org.hibernate.boot.registry.StandardServiceRegistry;
-import org.hibernate.engine.spi.SessionFactoryImplementor;
-import org.hibernate.service.ServiceRegistry;
-import org.hibernate.tool.schema.internal.ExceptionHandlerLoggedImpl;
-import org.hibernate.tool.schema.spi.ContributableMatcher;
-import org.hibernate.tool.schema.spi.ExceptionHandler;
-import org.hibernate.tool.schema.spi.ExecutionOptions;
-import org.hibernate.tool.schema.spi.SchemaFilter;
-import org.hibernate.tool.schema.spi.SchemaManagementTool;
-import org.hibernate.tool.schema.spi.SchemaValidator;
-import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
-import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
-import org.springframework.test.context.ActiveProfiles;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.ConfigurableApplicationContext;
 
-
-@DataJpaTest
-@ActiveProfiles("test")
-@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+@EntityScan(basePackages = {"co.dalicious.*"})
+@SpringBootApplication
 public class EntitySchemaValidationTest {
-
-  @Autowired
-  private EntityManagerFactory entityManagerFactory;
-
-  @Test
-  void validateSchema() {
-    SessionFactoryImplementor sessionFactory = entityManagerFactory.unwrap(SessionFactoryImplementor.class);
-    ServiceRegistry serviceRegistry = sessionFactory.getServiceRegistry();
-
-    SchemaManagementTool tool = serviceRegistry.getService(SchemaManagementTool.class);
-
-    Metadata metadata = new MetadataSources(serviceRegistry).buildMetadata();
-
-    Map<String, Object> configValues = new HashMap<>();
-    SchemaValidator schemaValidator = tool.getSchemaValidator(configValues);
-
-    ExecutionOptions options = new ExecutionOptions() {
-      @Override
-      public boolean shouldManageNamespaces() {
-        return true;
-      }
-
-      @Override
-      public Map<String, Object> getConfigurationValues() {
-        return configValues;
-      }
-
-      @Override
-      public ExceptionHandler getExceptionHandler() {
-        return ExceptionHandlerLoggedImpl.INSTANCE;
-      }
-
-      @Override
-      public SchemaFilter getSchemaFilter() {
-        return SchemaFilter.ALL;
-      }
-    };
-
-    schemaValidator.doValidation(metadata, options, ContributableMatcher.ALL);
+  public static void main(String[] args) {
+    ConfigurableApplicationContext ctx = SpringApplication.run(EntitySchemaValidationTest.class, args);
+    ctx.close();
   }
 }
