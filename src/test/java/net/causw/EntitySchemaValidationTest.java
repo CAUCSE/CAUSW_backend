cc1881adfc1565a6b8ae2e82c7cb379b94bdd7e5
GY102912 <qaz102912@gmail.com>
2025-06-12 23:24:29 +0900

    수정

---
+++ src/test/java/net/causw/EntitySchemaValidationTest.java
@@ -0,0 +1,13 @@
+package net.causw;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class EntitySchemaValidationTest {
+
+  @Test
+  void contextLoads() {
+    // Hibernate ddl-auto=validate 설정이 있으면, 여기서 검증됨
+  }
+}
