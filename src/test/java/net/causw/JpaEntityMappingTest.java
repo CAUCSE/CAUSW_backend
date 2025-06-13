3a12c361dbb9b93924b436270a2fccfbcf1c6d46
GY102912 <qaz102912@gmail.com>
2025-06-13 18:37:45 +0900

    설정 추가

--- src/test/java/net/causw/JpaEntityMappingTest.java
+++ src/test/java/net/causw/JpaEntityMappingTest.java
@@ -1,11 +1,13 @@
 package net.causw;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.test.context.ActiveProfiles;
 
 @DataJpaTest
 @ActiveProfiles("test")
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
 public class JpaEntityMappingTest {
 
   @Test
