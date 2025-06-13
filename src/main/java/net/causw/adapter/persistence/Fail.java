05f5cdf3e289c70e3544d9ee51ec830fc030c104
GY102912 <qaz102912@gmail.com>
2025-06-13 18:32:32 +0900

    테스트 실패

---
+++ src/main/java/net/causw/adapter/persistence/Fail.java
@@ -0,0 +1,22 @@
+package net.causw.adapter.persistence;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@Entity
+@Builder(access = AccessLevel.PROTECTED)
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+@Table(name = "tb_board")
+public class Fail {
+
+  @Id
+  private Long id;
+}
