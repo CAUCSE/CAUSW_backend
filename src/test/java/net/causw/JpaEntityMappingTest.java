15eff21c8bf782b829c7eaebf0c5f3e5f2fdcdbc
GY102912 <qaz102912@gmail.com>
2025-06-13 18:49:03 +0900

    springboottest로 변경

--- src/test/java/net/causw/JpaEntityMappingTest.java
+++ src/test/java/net/causw/JpaEntityMappingTest.java
@@ -2,10 +2,10 @@ package net.causw;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
-import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
 
-@DataJpaTest
+@SpringBootTest
 @ActiveProfiles("test")
 @AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
 public class JpaEntityMappingTest {
