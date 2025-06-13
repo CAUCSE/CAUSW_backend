74bda6836f52f6d98979d41dbfff9d7f528e5f6d
GY102912 <qaz102912@gmail.com>
2025-06-13 21:55:05 +0900

    컴포넌트 스캔 범위 제한

--- src/test/java/net/causw/JpaEntityMappingTest.java
+++ src/test/java/net/causw/JpaEntityMappingTest.java
@@ -1,13 +1,13 @@
 package net.causw;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
 @ActiveProfiles("test")
-@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+@ComponentScan(basePackages = "net.causw.adapter.persistence")
 public class JpaEntityMappingTest {
 
   @Test
