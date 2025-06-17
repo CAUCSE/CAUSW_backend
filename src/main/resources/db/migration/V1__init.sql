bec4ce1c43833d5591cf406c0d3aec03fb232fe3
GY102912 <qaz102912@gmail.com>
2025-06-17 14:26:32 +0900

    성공 케이스

--- src/main/resources/db/migration/V1__init.sql
+++ src/main/resources/db/migration/V1__init.sql
@@ -1602,6 +1602,17 @@ DEFAULT CHARACTER SET = utf8mb4
 COLLATE = utf8mb4_0900_ai_ci;
 
 
+-- -----------------------------------------------------
+-- Table `tb_fail`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `tb_fail` (
+    `user_id` VARCHAR(255) NOT NULL,
+    PRIMARY KEY (`user_id`))
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8mb4
+COLLATE = utf8mb4_0900_ai_ci;
+
+
 SET SQL_MODE=@OLD_SQL_MODE;
 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS;
 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS;
