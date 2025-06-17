cb2abe1e250b62b070d8178a316744e4d6b09097
GY102912 <qaz102912@gmail.com>
2025-06-18 00:17:23 +0900

    커밋 번호 수정 및 성공 케이스 커멘트 테스트

--- src/main/resources/db/migration/V20250617154230__CreateFailTable.sql
+++ src/main/resources/db/migration/V20250617154230__CreateFailTable.sql
@@ -1,6 +1,6 @@
 -- Migration: CreateFailTable
 
-CREATE TABLE `tb_board` (
-    `name` VARCHAR(255) NOT NULL,
-    PRIMARY KEY (`name`)
+CREATE TABLE `tb_fail` (
+    `id` VARCHAR(255) NOT NULL,
+    PRIMARY KEY (`id`)
 )
