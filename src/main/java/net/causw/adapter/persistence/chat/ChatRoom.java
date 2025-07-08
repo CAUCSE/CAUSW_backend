1fe959d5e9f604dac947dbef8f7156fc4b0b8276
hanjk0816 <hanjk0816@naver.com>
2025-07-08 17:03:50 +0900

    feat: Chatting관련 entity 작성 및 flyway 적용 완료

---
+++ src/main/java/net/causw/adapter/persistence/chat/ChatRoom.java
@@ -0,0 +1,45 @@
+package net.causw.adapter.persistence.chat;
+
+import jakarta.persistence.*;
+import lombok.*;
+import net.causw.adapter.persistence.base.BaseEntity;
+import net.causw.adapter.persistence.user.User;
+import net.causw.domain.model.enums.chat.ChatRoomType;
+import net.causw.domain.model.enums.chat.ParticipantRole;
+import java.util.HashSet;
+import java.util.Set;
+
+@Getter
+@Entity
+@Builder(access = AccessLevel.PROTECTED)
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+@Table(name = "tb_chat_room")
+public class ChatRoom extends BaseEntity {
+
+    @Column(name = "name", length = 100)
+    private String roomName;
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "type", nullable = false)
+    private ChatRoomType roomType;
+
+    @OneToMany(mappedBy = "chatRoom", cascade = CascadeType.ALL, orphanRemoval = true)
+    @Builder.Default
+    private Set<ChatRoomParticipant> participants = new HashSet<>();
+
+    public static ChatRoom of(String roomName, ChatRoomType roomType, User owner) {
+        ChatRoom room = ChatRoom.builder()
+                .roomName(roomName)
+                .roomType(roomType)
+                .build();
+        ChatRoomParticipant ownerParticipant = ChatRoomParticipant.of(room, owner, ParticipantRole.ADMIN);
+        room.addParticipant(ownerParticipant);
+        return room;
+    }
+
+    public void addParticipant(ChatRoomParticipant participant) {
+        participant.setChatRoom(this);
+        this.participants.add(participant);
+    }
+} 
---
+++ src/main/java/net/causw/domain/model/enums/chat/ParticipantRole.java
@@ -0,0 +1,6 @@
+package net.causw.domain.model.enums.chat;
+
+public enum ParticipantRole {
+    ADMIN,//그룹 채팅에서의 관리자
+    MEMBER // 일반 참여자
+} 
