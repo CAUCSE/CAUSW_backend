0845b734593132010f41978f16aed5c90f2c15f6
GY102912 <qaz102912@gmail.com>
2025-07-25 17:28:17 +0900

    remove: 회원가입시 학번 형식 검사 제거

--- app-main/src/main/java/net/causw/app/main/service/user/UserService.java
+++ app-main/src/main/java/net/causw/app/main/service/user/UserService.java
@@ -545,16 +545,7 @@ public class UserService {
 
     @Transactional
     public UserResponseDto signUp(UserCreateRequestDto userCreateRequestDto) {
-        // Make domain model for generalized data model and validate the format of request parameter
-
-        // 학번 앞 4자리와 입학년도가 다른 경우 잘못된 요청이므로 예외처리
-        if (!userCreateRequestDto.getStudentId().substring(0, 4).equals(userCreateRequestDto.getAdmissionYear().toString())) {
-            throw new BadRequestException(
-                    ErrorCode.INVALID_USER_DATA_REQUEST,
-                    MessageUtil.INVALID_USER_DATA_REQUEST
-            );
-        }
-
+        // email, nickname, studentId 중복 검사
         this.userRepository.findByEmail(userCreateRequestDto.getEmail()).ifPresent(
                 email -> {
                     throw new BadRequestException(
@@ -573,20 +564,21 @@ public class UserService {
                 }
         );
 
-        this.userRepository.findByStudentId(userCreateRequestDto.getStudentId()).ifPresent(
+        if (userCreateRequestDto.getStudentId() != null){
+            this.userRepository.findByStudentId(userCreateRequestDto.getStudentId()).ifPresent(
                 studentId -> {
                     throw new BadRequestException(
-                            ErrorCode.ROW_ALREADY_EXIST,
-                            MessageUtil.STUDENT_ID_ALREADY_EXIST
+                        ErrorCode.ROW_ALREADY_EXIST,
+                        MessageUtil.STUDENT_ID_ALREADY_EXIST
                     );
                 }
-        );
+            );
+        }
 
         User user = User.from(userCreateRequestDto, passwordEncoder.encode(userCreateRequestDto.getPassword()));
-
         this.userRepository.save(user);
 
-        // Validate password format, admission year range, and whether the email is duplicate or not
+        // password, admission year 값 등 검사
         ValidatorBucket.of()
                 .consistOf(ConstraintValidator.of(user, this.validator))
                 .consistOf(PasswordFormatValidator.of(userCreateRequestDto.getPassword()))
