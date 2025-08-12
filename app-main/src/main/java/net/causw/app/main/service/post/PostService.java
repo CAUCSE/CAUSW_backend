4e968be83d4a8ff9916fe8ba1a12a160c7898872
riadan710 <riadan710@gmail.com>
2025-08-12 19:45:42 +0900

    fix: validator 사용

--- app-main/src/main/java/net/causw/app/main/service/post/PostService.java
+++ app-main/src/main/java/net/causw/app/main/service/post/PostService.java
@@ -110,15 +110,8 @@ public class PostService {
     public PostResponseDto findPostById(User user, String postId) {
         Post post = getPost(postId);
 
-        // 삭제된 게시글 열람 차단
-        if (isPostDeleted(post)) {
-            throw new BadRequestException(
-                    ErrorCode.TARGET_DELETED,
-                    MessageUtil.POST_DELETED
-            );
-        }
-
         ValidatorBucket validatorBucket = initializeValidator(user, post.getBoard());
+        validatorBucket.consistOf(TargetIsDeletedValidator.of(post.getIsDeleted(), StaticValue.DOMAIN_POST));
         validatorBucket.validate();
 
         PostResponseDto postResponseDto = toPostResponseDtoExtended(post, user);
