202e9eef4587dd2d9779662f5e160b979f78b6a8
LEE GAYEON <150024627+GY102912@users.noreply.github.com>
2025-08-16 15:16:30 +0900

    fix: 동문수첩 프로필 목록 비활성 유저 필터링 (#925)
    * fix: 동문수첩 프로필 목록 비활성 유저 필터링
    
    * remove: 관련 없는 파일 삭제
    
    * remove: 불필요한 에러 메시지 삭제
    
    * remove: 메소드명 언더바 제거

--- app-main/src/main/java/net/causw/app/main/repository/userInfo/UserInfoRepository.java
+++ app-main/src/main/java/net/causw/app/main/repository/userInfo/UserInfoRepository.java
@@ -2,6 +2,7 @@ package net.causw.app.main.repository.userInfo;
 
 import java.util.Optional;
 import net.causw.app.main.domain.model.entity.userInfo.UserInfo;
+import net.causw.app.main.domain.model.enums.user.UserState;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -14,15 +15,16 @@ public interface UserInfoRepository extends JpaRepository<UserInfo, String> {
 
     Optional<UserInfo> findByUserId(String userId);
 
-    Page<UserInfo> findAllByOrderByUpdatedAtDesc(Pageable pageable);
+    Page<UserInfo> findAllByUserStateOrderByUpdatedAtDesc(UserState state, Pageable pageable);
 
     @Query("""
     SELECT DISTINCT ui FROM UserInfo ui
     LEFT JOIN ui.userCareer uc
-    WHERE uc.description LIKE CONCAT('%', :keyword, '%')
+    WHERE (uc.description LIKE CONCAT('%', :keyword, '%')
     OR ui.user.name LIKE CONCAT('%', :keyword, '%')
-    OR ui.job LIKE CONCAT('%', :keyword, '%')
+    OR ui.job LIKE CONCAT('%', :keyword, '%'))
+    AND ui.user.state = :state
     ORDER BY ui.updatedAt DESC
     """)
-    Page<UserInfo> findAllByKeywordInNameOrJobOrCareer(@Param("keyword") String keyword, Pageable pageable);
+    Page<UserInfo> findAllByUserStateAndKeywordInNameOrJobOrCareer(@Param("state") UserState state, @Param("keyword") String keyword, Pageable pageable);
 }
