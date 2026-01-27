ccbeda63d33401eae38e22614fd06f1d2f0b64e1
bingle625 <fhqhxm8899@gmail.com>
2026-01-27 12:13:43 +0900

    feat: 예외처리 추가

--- app-main/src/main/java/net/causw/app/main/domain/asset/locker/api/v2/controller/admin/LockerAdminController.java
+++ app-main/src/main/java/net/causw/app/main/domain/asset/locker/api/v2/controller/admin/LockerAdminController.java
@@ -4,28 +4,24 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import net.causw.app.main.core.global.annotation.V2Api;
-import net.causw.global.constant.MessageUtil;
-import net.causw.global.exception.BadRequestException;
-import net.causw.global.exception.ErrorCode;
+import net.causw.app.main.shared.dto.ApiResponse;
+import net.causw.app.main.shared.exception.BaseRunTimeV2Exception;
+import net.causw.app.main.shared.exception.errorcode.AuthErrorCode;
 
 import lombok.RequiredArgsConstructor;
 
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/v2/admin/lockers")
-@V2Api
 public class LockerAdminController {
 
 	@GetMapping("/schedules/current")
-	public String getCurrentLockerSchedule() {
+	public ApiResponse<Void> getCurrentLockerSchedule() {
 
 		if (true) {
-			throw new BadRequestException(
-				ErrorCode.INVALID_EXPIRE_DATE,
-				MessageUtil.LOCKER_ALREADY_EXTENDED);
+			throw new BaseRunTimeV2Exception(AuthErrorCode.INVALID_TOKEN);
 		}
-		return "test";
+		return ApiResponse.success();
 	}
 
 }
