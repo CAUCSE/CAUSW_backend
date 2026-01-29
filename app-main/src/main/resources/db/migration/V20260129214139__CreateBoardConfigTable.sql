112331a7d909001bbec6c6026dac26b6af5ee2ed
bingle625 <fhqhxm8899@gmail.com>
2026-01-29 21:49:48 +0900

    feat: board config에서 boardId에 대한 unique 제약조건 추가

--- app-main/src/main/resources/db/migration/V20260129214139__CreateBoardConfigTable.sql
+++ app-main/src/main/resources/db/migration/V20260129214139__CreateBoardConfigTable.sql
@@ -1,25 +1,3 @@
--- Migration: CreateBoardConfigTable
-/**
-  public class BoardConfig extends BaseEntity {
-
-	private String boardId;
-
-	@Column(name = "is_anonymous", nullable = false)
-	private boolean isAnonymous;
-
-	@Column(name = "read_scope", nullable = false)
-	private BoardReadScope readScope;
-
-	@Column(name = "write_scope", nullable = false)
-	private BoardWriteScope writeScope;
-
-	@Column(name = "is_notice", nullable = false)
-	private boolean isNotice;
-
-	@Column(name = "visibility", nullable = false)
-	private BoardVisibility visibility;
-}
- */
 CREATE TABLE tb_board_config (
     id varchar(255) NOT NULL,
     board_id VARCHAR(255) NOT NULL,
@@ -31,3 +9,6 @@ CREATE TABLE tb_board_config (
     created_at datetime(6) DEFAULT NULL,
     updated_at datetime(6) DEFAULT NULL
 );
+
+ALTER TABLE tb_board_config
+ADD CONSTRAINT uq_board_id UNIQUE (board_id);
