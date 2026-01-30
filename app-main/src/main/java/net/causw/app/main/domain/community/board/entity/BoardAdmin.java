c7b8b4764af480bda6c23c668b78758086f3783c
bingle625 <fhqhxm8899@gmail.com>
2026-01-30 16:58:35 +0900

    feat: 게시판 관리자 매핑 테이블 추가
    게시판 관리자와 사용자 간의 관계를 나타내는 BoardAdmin 엔티티를 추가합니다.
    
    BoardAdmin 엔티티는 게시판 ID와 사용자 ID를 저장하여 게시판 관리 권한을 관리합니다.

---
+++ app-main/src/main/java/net/causw/app/main/domain/community/board/entity/BoardAdmin.java
@@ -0,0 +1,35 @@
+package net.causw.app.main.domain.community.board.entity;
+
+import net.causw.app.main.shared.entity.BaseEntityWithoutId;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@Entity
+@Builder(access = AccessLevel.PROTECTED)
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+@Table(name = "tb_board_admin")
+public class BoardAdmin extends BaseEntityWithoutId {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "id", nullable = false, unique = true)
+	private Long id;
+
+	@Column(name = "board_id", nullable = false)
+	private String boardId;
+
+	@Column(name = "user_id", nullable = false)
+	private String userId;
+}
