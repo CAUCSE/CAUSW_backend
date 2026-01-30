448a2b7d05164d937100f695bd13db1c06a935e7
KEEKE132 <dksvlfdhs1@gmail.com>
2026-01-30 21:26:36 +0900

    feat: dto에 swagger 명세 추가

--- app-main/src/main/java/net/causw/app/main/domain/campus/schedule/api/v2/controller/dto/response/ScheduleListResponse.java
+++ app-main/src/main/java/net/causw/app/main/domain/campus/schedule/api/v2/controller/dto/response/ScheduleListResponse.java
@@ -2,10 +2,13 @@ package net.causw.app.main.domain.campus.schedule.api.v2.controller.dto.response
 
 import java.util.List;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Builder;
 
 @Builder
+@Schema(description = "일정 목록 응답")
 public record ScheduleListResponse(
-	Integer count,
-	List<ScheduleResponse> data) {
+	@Schema(description = "조회된 일정 개수", example = "3") Integer count,
+
+	@Schema(description = "일정 목록") List<ScheduleResponse> data) {
 }
