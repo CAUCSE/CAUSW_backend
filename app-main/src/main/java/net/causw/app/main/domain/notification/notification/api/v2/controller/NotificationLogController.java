c6f76c7188ef83df3c4864c707a9c027a02ee636
ImJaewook <bigthinking33@gmail.com>
2026-01-31 21:21:22 +0900

    api response 적용

--- app-main/src/main/java/net/causw/app/main/domain/notification/notification/api/v2/controller/NotificationLogController.java
+++ app-main/src/main/java/net/causw/app/main/domain/notification/notification/api/v2/controller/NotificationLogController.java
@@ -11,6 +11,7 @@ import net.causw.app.main.domain.notification.notification.api.v2.dto.response.N
 import net.causw.app.main.domain.notification.notification.api.v2.dto.response.NotificationResponseDto;
 import net.causw.app.main.domain.notification.notification.service.NotificationLogService;
 import net.causw.app.main.domain.user.auth.userdetails.CustomUserDetails;
+import net.causw.app.main.shared.dto.ApiResponse;
 
 import io.swagger.v3.oas.annotations.Operation;
 import lombok.RequiredArgsConstructor;
@@ -25,20 +26,24 @@ public class NotificationLogController {
 	@ResponseStatus(HttpStatus.OK)
 	@Operation(summary = "유저에게 온 최신 알람 조회", description = "유저의 최신 알림을 조회합니다. <br>" +
 		"해당 api는 홈 화면에서 읽지 않은 최신 알람 1개를 표시할 때 사용됩니다.")
-	public NotificationResponseDto getNotificationTop1(
+	public ApiResponse<NotificationResponseDto> getNotificationTop1(
 		@AuthenticationPrincipal
 		CustomUserDetails userDetails) {
-		return notificationLogService.getNotificationTop1(userDetails.getUser());
+		NotificationResponseDto response = notificationLogService.getNotificationTop1(userDetails.getUser());
+
+		return ApiResponse.success(response);
 	}
 
 	@GetMapping("/count")
 	@ResponseStatus(value = HttpStatus.OK)
 	@Operation(summary = "유저에게 온 일반, 경조사 알림 중 읽지 않은 알림 총 개수 반환", description = "유저의 읽지 않은 알림 개수를 반환합니다.<br>" +
 		"UI 상에서 10개 이상은 9+로 표기되기 때문에 10개까지 카운팅 되도록 하였습니다.")
-	public NotificationCountResponseDto getNotificationLogCount(
+	public ApiResponse<NotificationCountResponseDto> getNotificationLogCount(
 		@AuthenticationPrincipal
 		CustomUserDetails userDetails) {
-		return notificationLogService.getNotificationLogCount(userDetails.getUser());
+		NotificationCountResponseDto response = notificationLogService.getNotificationLogCount(userDetails.getUser());
+
+		return ApiResponse.success(response);
 	}
 
 }
