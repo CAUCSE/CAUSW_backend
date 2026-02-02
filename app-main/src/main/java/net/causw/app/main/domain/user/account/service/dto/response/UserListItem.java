e731b5454ca1f8e514260cd8cf509a710c0c5349
ygcho <ygcho21@gmail.com>
2026-02-02 16:18:50 +0900

    chore: 사용 안하는 import문 제거

--- app-main/src/main/java/net/causw/app/main/domain/user/account/service/dto/response/UserListItem.java
+++ app-main/src/main/java/net/causw/app/main/domain/user/account/service/dto/response/UserListItem.java
@@ -1,13 +1,11 @@
 package net.causw.app.main.domain.user.account.service.dto.response;
 
-import com.querydsl.core.annotations.QueryProjection;
 import net.causw.app.main.domain.user.academic.enums.userAcademicRecord.AcademicStatus;
 import net.causw.app.main.domain.user.account.entity.user.User;
 import net.causw.app.main.domain.user.account.enums.user.Department;
 import net.causw.app.main.domain.user.account.enums.user.UserState;
 
 import java.time.LocalDateTime;
-import java.util.UUID;
 
 public record UserListItem(
         String id,
