c111007bd892050f2f2454e30b6ff44a76f3cda6
ImJaewook <bigthinking33@gmail.com>
2026-02-03 17:28:26 +0900

    refactor: convert NotificationResponseDto to record

--- app-main/src/main/java/net/causw/app/main/domain/notification/notification/api/v2/dto/response/NotificationCountResponseDto.java
+++ app-main/src/main/java/net/causw/app/main/domain/notification/notification/api/v2/dto/response/NotificationCountResponseDto.java
@@ -1,16 +1,12 @@
 package net.causw.app.main.domain.notification.notification.api.v2.dto.response;
 
 import io.swagger.v3.oas.annotations.media.Schema;
-import lombok.AllArgsConstructor;
 import lombok.Builder;
-import lombok.Getter;
-import lombok.Setter;
 
-@Getter
-@Setter
+
+
 @Builder
-@AllArgsConstructor
-public class NotificationCountResponseDto {
+public record NotificationCountResponseDto(
 	@Schema(description = "읽지 않은 알림 개수", example = "1, 2, 3,...")
-	private Integer notificationLogCount;
+	Integer notificationLogCount) {
 }
