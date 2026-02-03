2ee4c3d77d381e6f9aed1ee1d09ceefc5ea61071
ImJaewook <bigthinking33@gmail.com>
2026-02-03 17:30:27 +0900

    refactor: apply spotless

--- app-main/src/main/java/net/causw/app/main/domain/notification/notification/api/v2/dto/response/NotificationCountResponseDto.java
+++ app-main/src/main/java/net/causw/app/main/domain/notification/notification/api/v2/dto/response/NotificationCountResponseDto.java
@@ -3,10 +3,7 @@ package net.causw.app.main.domain.notification.notification.api.v2.dto.response;
 import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Builder;
 
-
-
 @Builder
 public record NotificationCountResponseDto(
-	@Schema(description = "읽지 않은 알림 개수", example = "1, 2, 3,...")
-	Integer notificationLogCount) {
+	@Schema(description = "읽지 않은 알림 개수", example = "1, 2, 3,...") Integer notificationLogCount) {
 }
