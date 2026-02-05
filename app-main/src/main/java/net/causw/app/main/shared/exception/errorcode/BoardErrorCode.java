1c47ecfa3c8e58a170fb8336ce177dc0eabbf767
KEEKE132 <dksvlfdhs1@gmail.com>
2026-02-06 02:06:24 +0900

    feat: BoardErrorCode 추가

--- app-main/src/main/java/net/causw/app/main/shared/exception/errorcode/BoardErrorCode.java
+++ app-main/src/main/java/net/causw/app/main/shared/exception/errorcode/BoardErrorCode.java
@@ -10,7 +10,8 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor(access = AccessLevel.PROTECTED)
 public enum BoardErrorCode implements BaseResponseCode {
 	BOARD_NOT_FOUND(HttpStatus.NOT_FOUND, "BOARD_404_001", "게시판을 찾을 수 없습니다"),
-	BOARD_NAME_DUPLICATE(HttpStatus.BAD_REQUEST, "BOARD_400_001", "이미 사용 중인 게시판 이름입니다.");
+	BOARD_NAME_DUPLICATE(HttpStatus.BAD_REQUEST, "BOARD_400_001", "이미 사용 중인 게시판 이름입니다."),
+	BOARD_FORBIDDEN(HttpStatus.FORBIDDEN, "BOARD_403_001", "게시판에 대한 권한이 없습니다");
 
 	private final HttpStatus status;
 	private final String code;
