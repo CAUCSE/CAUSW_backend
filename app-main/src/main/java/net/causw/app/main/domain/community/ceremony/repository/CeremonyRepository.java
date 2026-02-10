8f493dc7370c46202554b685bfa01eb153c9f7f3
hyungin0505 <hyungin0505@naver.com>
2026-02-10 15:57:48 +0900

    feat: 경조사 목록 조회 repository 추가

--- app-main/src/main/java/net/causw/app/main/domain/community/ceremony/repository/CeremonyRepository.java
+++ app-main/src/main/java/net/causw/app/main/domain/community/ceremony/repository/CeremonyRepository.java
@@ -1,21 +1,96 @@
 package net.causw.app.main.domain.community.ceremony.repository;
 
-import java.util.Optional;
+import java.time.LocalDate;
+import java.time.LocalTime;
 
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import net.causw.app.main.domain.community.ceremony.entity.Ceremony;
 import net.causw.app.main.domain.community.ceremony.enums.CeremonyState;
-import net.causw.app.main.domain.user.account.entity.user.User;
 
 public interface CeremonyRepository extends JpaRepository<Ceremony, String> {
-	Page<Ceremony> findAllByUserAndCeremonyStateOrderByCreatedAtDesc(User user, CeremonyState ceremonyState,
+
+	@Query("SELECT c " +
+		"FROM Ceremony c " +
+		"WHERE (c.ceremonyState = 'ACCEPT') " +
+		"ORDER BY c.startDate, c.startTime ASC")
+	Page<Ceremony> findAllOrderByStartedAtAsc(Pageable pageable);
+
+	@Query("SELECT c " +
+		"FROM Ceremony c " +
+		"WHERE (c.ceremonyState = 'ACCEPT' AND c.ceremonyType = :type)" +
+		"AND ((c.startDate < :nowDate AND :nowDate < c.endDate) " +
+		"OR ((c.startDate = :nowDate AND c.endDate = :nowDate) AND (c.startTime <= :nowTime AND :nowTime <= c.endTime)) "
+		+
+		"OR ((c.startDate = :nowDate AND :nowDate < c.endDate) AND (c.startTime <= :nowTime)) " +
+		"OR ((c.startDate < :nowDate AND c.endDate = :nowDate) AND (:nowTime <= c.endTime)) " +
+		"OR ((c.startDate = :nowDate AND c.endDate = :nowDate) AND c.startTime IS NULL)) " +
+		"ORDER BY c.startTime ASC")
+	Page<Ceremony> findOngoingByTypeOrderByStartedAtAsc(
+		@Param("type") String type, @Param("nowDate") LocalDate nowDate, @Param("nowTime") LocalTime nowTime,
 		Pageable pageable);
 
-	Page<Ceremony> findByCeremonyStateOrderByCreatedAtDesc(CeremonyState ceremonyState, Pageable pageable);
+	@Query("SELECT c " +
+		"FROM Ceremony c " +
+		"WHERE (c.ceremonyState = 'ACCEPT' AND c.ceremonyType = :type) " +
+		"AND ((:nowDate < c.startDate AND c.startDate <= :toDate) " +
+		"OR (c.startDate = :nowDate AND (c.startTime IS NOT NULL AND :nowTime < c.startTime))) " +
+		"ORDER BY c.startDate, c.startTime ASC")
+	Page<Ceremony> findUpcomingByTypeOrderByStartedAtAsc(
+		@Param("type") String type, @Param("nowDate") LocalDate nowDate, @Param("nowTime") LocalTime nowTime,
+		@Param("toDate") LocalDate toDate, Pageable pageable);
 
-	Optional<Ceremony> findByIdAndUser(String id, User user);
+	@Query("SELECT c " +
+		"FROM Ceremony c " +
+		"WHERE (c.ceremonyState = 'ACCEPT' AND c.ceremonyType = :type) " +
+		"AND ((:fromDate <= c.endDate AND c.endDate < :nowDate) " +
+		"OR (c.endDate = :nowDate AND (c.endTime IS NOT NULL AND c.endTime < :nowTime))) " +
+		"ORDER BY c.endDate, c.endTime ASC")
+	Page<Ceremony> findPastByTypeOrderByEndedAtAsc(
+		@Param("type") String type, @Param("nowDate") LocalDate nowDate, @Param("nowTime") LocalTime nowTime,
+		@Param("fromDate") LocalDate fromDate, Pageable pageable);
 
+	@Query("SELECT c " +
+		"FROM Ceremony c " +
+		"WHERE c.ceremonyState = 'ACCEPT' " +
+		"AND ((c.startDate < :nowDate AND :nowDate < c.endDate) " +
+		"OR ((c.startDate = :nowDate AND c.endDate = :nowDate) AND (c.startTime <= :nowTime AND :nowTime <= c.endTime)) "
+		+
+		"OR ((c.startDate = :nowDate AND :nowDate < c.endDate) AND (c.startTime <= :nowTime)) " +
+		"OR ((c.startDate < :nowDate AND c.endDate = :nowDate) AND (:nowTime <= c.endTime)) " +
+		"OR ((c.startDate = :nowDate AND c.endDate = :nowDate) AND c.startTime IS NULL)) " +
+		"ORDER BY c.startTime ASC")
+	Page<Ceremony> findAllOngoingOrderByStartedAtAsc(
+		@Param("nowDate") LocalDate nowDate, @Param("nowTime") LocalTime nowTime, Pageable pageable);
+
+	@Query("SELECT c " +
+		"FROM Ceremony c " +
+		"WHERE c.ceremonyState = 'ACCEPT' " +
+		"AND ((:nowDate < c.startDate AND c.startDate <= :toDate) " +
+		"OR (c.startDate = :nowDate AND (c.startTime IS NOT NULL AND :nowTime < c.startTime))) " +
+		"ORDER BY c.startDate, c.startTime ASC")
+	Page<Ceremony> findAllUpcomingOrderByStartedAtAsc(
+		@Param("nowDate") LocalDate nowDate, @Param("nowTime") LocalTime nowTime, @Param("toDate") LocalDate toDate,
+		Pageable pageable);
+
+	@Query("SELECT c " +
+		"FROM Ceremony c " +
+		"WHERE c.ceremonyState = 'ACCEPT' " +
+		"AND ((:fromDate <= c.endDate AND c.endDate < :nowDate) " +
+		"OR (c.endDate = :nowDate AND (c.endTime IS NOT NULL AND c.endTime < :nowTime))) " +
+		"ORDER BY c.endDate, c.endTime ASC")
+	Page<Ceremony> findAllPastOrderByEndedAtAsc(
+		@Param("nowDate") LocalDate nowDate, @Param("nowTime") LocalTime nowTime, @Param("fromDate") LocalDate fromDate,
+		Pageable pageable);
+
+	@Query("SELECT c " +
+		"FROM Ceremony c " +
+		"WHERE (c.user.id = :userId AND (:state IS NULL OR c.ceremonyState = :state)) " +
+		"ORDER BY c.startDate, c.startTime ASC")
+	Page<Ceremony> findMyByStateOrderByStartedAtAsc(@Param("userId") String userId, @Param("state") CeremonyState state,
+		Pageable pageable);
 }
--- app-main/src/main/java/net/causw/app/main/domain/community/ceremony/service/implementation/CeremonyReader.java
+++ app-main/src/main/java/net/causw/app/main/domain/community/ceremony/service/implementation/CeremonyReader.java
@@ -1,11 +1,16 @@
 package net.causw.app.main.domain.community.ceremony.service.implementation;
 
+import java.time.LocalDate;
+import java.time.LocalTime;
 import java.util.Optional;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Component;
 import org.springframework.transaction.annotation.Transactional;
 
 import net.causw.app.main.domain.community.ceremony.entity.Ceremony;
+import net.causw.app.main.domain.community.ceremony.enums.CeremonyState;
 import net.causw.app.main.domain.community.ceremony.repository.CeremonyRepository;
 
 import lombok.RequiredArgsConstructor;
@@ -19,5 +24,42 @@ public class CeremonyReader {
 
 	public Optional<Ceremony> findById(String ceremonyId) {
 		return ceremonyRepository.findById(ceremonyId);
+	}
+
+	public Page<Ceremony> findAllOrderByStartedAtAsc(Pageable pageable) {
+		return ceremonyRepository.findAllOrderByStartedAtAsc(pageable);
+	}
+
+	public Page<Ceremony> findOngoingByTypeOrderByStartedAtAsc(String type, LocalDate nowDate, LocalTime nowTime,
+		Pageable pageable) {
+		return ceremonyRepository.findOngoingByTypeOrderByStartedAtAsc(type, nowDate, nowTime, pageable);
+	}
+
+	public Page<Ceremony> findUpcomingByTypeOrderByStartedAtAsc(String type, LocalDate nowDate, LocalTime nowTime,
+		LocalDate toDate, Pageable pageable) {
+		return ceremonyRepository.findUpcomingByTypeOrderByStartedAtAsc(type, nowDate, nowTime, toDate, pageable);
+	}
+
+	public Page<Ceremony> findPastByTypeOrderByEndedAtAsc(String type, LocalDate nowDate, LocalTime nowTime,
+		LocalDate fromDate, Pageable pageable) {
+		return ceremonyRepository.findPastByTypeOrderByEndedAtAsc(type, nowDate, nowTime, fromDate, pageable);
+	}
+
+	public Page<Ceremony> findAllOngoingOrderByStartedAtAsc(LocalDate nowDate, LocalTime nowTime, Pageable pageable) {
+		return ceremonyRepository.findAllOngoingOrderByStartedAtAsc(nowDate, nowTime, pageable);
+	}
+
+	public Page<Ceremony> findAllUpcomingOrderByStartedAtAsc(LocalDate nowDate, LocalTime nowTime, LocalDate toDate,
+		Pageable pageable) {
+		return ceremonyRepository.findAllUpcomingOrderByStartedAtAsc(nowDate, nowTime, toDate, pageable);
+	}
+
+	public Page<Ceremony> findAllPastOrderByEndedAtAsc(LocalDate nowDate, LocalTime nowTime, LocalDate fromDate,
+		Pageable pageable) {
+		return ceremonyRepository.findAllPastOrderByEndedAtAsc(nowDate, nowTime, fromDate, pageable);
+	}
+
+	public Page<Ceremony> findMyByStateOrderByStartedAtAsc(String userId, CeremonyState state, Pageable pageable) {
+		return ceremonyRepository.findMyByStateOrderByStartedAtAsc(userId, state, pageable);
 	}
 }
