0ea910380841c5a716a08202bb2606337ee268f7
KoGandhi <hyungin0505@naver.com>
2026-02-10 19:44:09 +0900

    fix: flyway 스크립트 수정 (#1075)
    * feat: 경조사 신청 및 상세 보기 API 개발
    
    * feat: 경조사 종료일 없이 종료 시간 입력된 경우 에러 처리
    
    * refactor: 경조사 service v2 디렉토리 제거
    
    * feat: flyway 엔티티 null 조건 맞춰 수정
    
    ceremony_category ENUM에서 String으로 변경, family_relation ENUM 오타 수정, relation_type 기존 데이터 임의로 채워서 NOT NULL 설정
    
    * feat: CeremonyService 관계 검증 조건문 수정
    
    Transactional 어노테이션 추가 및 user 일치 여부 확인 로직 개선
    
    * feat: 경조사 생성 dto에 학번 검증 추가
    
    * feat: 테스트 코드 수정
    
    CeremonyNotificationServiceTest에서 경조사 상세 분류를 문자열로 올바르게 전달
    
    * feat: 경조사 이미지 경로 생성 및 적용
    
    * feat: v1 api와 v2 api 격리
    
    기존 서비스 로직 유지하기 위해 Ceremony 엔티티에 ofV1, createWithImagesV1 추가하였습니다
    
    * chore: 오탈자 및 포매팅 정리
    
    * chore: 사용하지 않는 import문 제거
    
    * feat: 동문 상세 관계 검증 조건 수정
    
    * refactor: implementation 레이어 추가
    
    service에서 repository 직접 사용하지 않도록 implementation 레이어 추가하였습니다
    
    * chore: spotless 적용
    
    * feat: 경조사 서비스 테스트 코드 추가
    
    * refactor: dto 하위 패키지로 response와 request dto 분리
    
    * feat: 문의 연락처 입력 형식 검증 추가
    
    * feat: 에러코드 수정
    
    * feat: nullable 칼럼 null 적용되도록 flyway 스크립트 내용 추가
    
    description, ceremony_category, end_date에 NULL 적용
    
    * feat: 에러 처리 개선
    
    * feat: 날짜 및 시간 형식 검증 분리
    
    * feat: 서비스 테스트 로직 수정
    
    * feat: 상세 분류 필드를 enum과 직접 입력 string 필드로 분리
    
    * feat: 종료 시간 입력 시 시작 시간 입력되어있는지 검증 로직 추가
    
    * feat: 테스트 코드 수정 및 플로우 순서대로 검증하도록 검증 순서 변경
    
    * refactor: mapper 클래스 분리
    
    * refactor: 서비스 검증 로직 validator로 분리
    
    * feat: 테스트 코드 수정
    
    * feat: 동문소식 대신 전달 관계 enum 값 수정
    
    * feat: validator 로직 개선
    
    * feat: 카테고리 및 관계 enum에서 string으로 변경
    
    * chore: 불필요한 import 제거
    
    * feat: v1 서비스 코드 수정
    
    * feat: flyway 스크립트 작성
    
    * feat: 이미 끝난 경조사 생성 시 에러 처리
    
    * chore: 오탈자 수정
    
    * feat: npe 나는 에러 처리 로직 수정
    
    * feat: 서비스 코드 수정
    
    * feat: 상세 분류 직접 입력 필드 추가
    
    * feat: 승인되지 않은 경조사는 general 상세 보기 불가능하도록 수정
    
    * feat: 테스트 코드 수정
    
    * feat: 기존 v1 알림 코드대로 변경
    
    * Revert "feat: 경조사 신청 및 상세 보기 v2 api 개발 (#1061)"
    
    This reverts commit 4c2328511d28b403d5adad716553b480eec6b0fe.
    
    * feat: flyway 스크립트 수정

--- app-main/src/main/resources/db/migration/V20260128233054__ModifyCeremonyTableScheme.sql
+++ app-main/src/main/resources/db/migration/V20260128233054__ModifyCeremonyTableScheme.sql
@@ -9,7 +9,7 @@ ALTER TABLE tb_ceremony
     ADD COLUMN ceremony_custom_category VARCHAR(50) NULL AFTER ceremony_category,
     ADD COLUMN end_time TIME NULL AFTER start_date,
     ADD COLUMN start_time TIME NULL AFTER end_time,
-    ADD COLUMN relation_type ENUM('ME', 'FAMILY', 'ALUMNI') NULL AFTER user_id,
+    ADD COLUMN relation_type ENUM('ME', 'FAMILY', 'INSTEAD') NULL AFTER user_id,
     ADD COLUMN family_relation VARCHAR(20) NULL AFTER relation_type,
     ADD COLUMN alumni_relation VARCHAR(20) NULL AFTER family_relation,
     ADD COLUMN alumni_name VARCHAR(20) NULL AFTER alumni_relation,
@@ -25,8 +25,8 @@ ALTER TABLE tb_ceremony
 UPDATE tb_ceremony
 SET ceremony_type =
     CASE
-        WHEN ceremony_category = 'FUNERAL' THEN '조사'
-        ELSE '경사'
+        WHEN ceremony_category = 'FUNERAL' THEN 'CONDOLENCE'
+        ELSE 'CELEBRATION'
     END
 WHERE ceremony_type IS NULL;
 
