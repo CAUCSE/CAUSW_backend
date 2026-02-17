78986bfcde39f5fc3f26c75920a8dd633d11f63c
bingle625 <fhqhxm8899@gmail.com>
2026-02-17 20:09:38 +0900

    fix:typo

--- app-main/src/main/resources/db/migration/V20260213120000__AddDescriptionToLockerLocation.sql
+++ app-main/src/main/resources/db/migration/V20260213120000__AddDescriptionToLockerLocation.sql
@@ -1,8 +1,8 @@
 -- Migration: AddDescriptionToLockerLocation
 
-ALTER TABLE tb_local_location
+ALTER TABLE tb_locker_location
 ADD COLUMN description VARCHAR(255) NULL;
 
-UPDATE tb_local_location SET description = '2층' WHERE name = 'SECOND';
-UPDATE tb_local_location SET description = '3층' WHERE name = 'THIRD';
-UPDATE tb_local_location SET description = '4층' WHERE name = 'FOURTH';
+UPDATE tb_locker_location SET description = '2층' WHERE name = 'SECOND';
+UPDATE tb_locker_location SET description = '3층' WHERE name = 'THIRD';
+UPDATE tb_locker_location SET description = '4층' WHERE name = 'FOURTH';
