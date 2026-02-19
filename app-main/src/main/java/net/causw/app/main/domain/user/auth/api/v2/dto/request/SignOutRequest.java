485946a069453a9011afd5131a2e00c26b8b359f
hyoinkang <hyoin1704@gmail.com>
2026-02-19 23:21:36 +0900

    docs: 로그아웃 reqeust dto에 대한 명세 추가

--- app-main/src/main/java/net/causw/app/main/domain/user/auth/api/v2/dto/request/SignOutRequest.java
+++ app-main/src/main/java/net/causw/app/main/domain/user/auth/api/v2/dto/request/SignOutRequest.java
@@ -1,5 +1,8 @@
 package net.causw.app.main.domain.user.auth.api.v2.dto.request;
 
+import io.swagger.v3.oas.annotations.media.Schema;
+
+@Schema(description = "로그아웃 요청 DTO")
 public record SignOutRequest(
-	String fcmToken) {
+	@Schema(description = "삭제할 기기의 FCM (푸시 알림) 토큰") String fcmToken) {
 }
