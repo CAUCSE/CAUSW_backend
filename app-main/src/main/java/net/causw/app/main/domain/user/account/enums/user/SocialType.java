a4ab64ed2a2b9c91dd3718ad5f1a607425c8acb5
hyoinkang <hyoin1704@gmail.com>
2026-02-21 22:39:18 +0900

    fix: Enum 매칭 예외 수정

--- app-main/src/main/java/net/causw/app/main/domain/user/account/enums/user/SocialType.java
+++ app-main/src/main/java/net/causw/app/main/domain/user/account/enums/user/SocialType.java
@@ -1,11 +1,18 @@
 package net.causw.app.main.domain.user.account.enums.user;
 
+import java.util.Arrays;
+
+import net.causw.app.main.shared.exception.errorcode.AuthErrorCode;
+
 public enum SocialType {
 	GOOGLE,
 	APPLE,
 	KAKAO;
 
 	public static SocialType from(String registrationId) {
-		return SocialType.valueOf(registrationId.toUpperCase());
+		return Arrays.stream(SocialType.values())
+			.filter(type -> type.name().equalsIgnoreCase(registrationId))
+			.findFirst()
+			.orElseThrow(AuthErrorCode.UNSUPPORTED_SOCIAL_PROVIDER::toBaseException);
 	}
 }
