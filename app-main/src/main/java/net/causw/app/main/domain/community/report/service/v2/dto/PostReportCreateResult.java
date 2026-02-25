1c7e9996d93ffd83d32ec4339e099c98c5010fbf
sopp1313 <dldpdms0131@gmail.com>
2026-02-25 16:31:08 +0900

    refactor: 응답 최적화

--- app-main/src/main/java/net/causw/app/main/domain/community/report/service/v2/dto/PostReportCreateResult.java
+++ app-main/src/main/java/net/causw/app/main/domain/community/report/service/v2/dto/PostReportCreateResult.java
@@ -2,6 +2,7 @@ package net.causw.app.main.domain.community.report.service.v2.dto;
 
 import java.time.LocalDateTime;
 
+import net.causw.app.main.domain.community.report.entity.Report;
 import net.causw.app.main.domain.community.report.enums.ReportReason;
 
 import lombok.Builder;
@@ -12,4 +13,12 @@ public record PostReportCreateResult(
 	String postId,
 	ReportReason reportReason,
 	LocalDateTime createdAt) {
+
+	public static PostReportCreateResult from(Report report) {
+		return new PostReportCreateResult(
+			report.getId(),
+			report.getTargetId(),
+			report.getReportReason(),
+			report.getCreatedAt());
+	}
 }
