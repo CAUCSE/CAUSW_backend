9c1df7fcb598731dca855365d56723b513f94cf9
KEEKE132 <dksvlfdhs1@naver.com>
2026-02-27 20:46:55 +0900

    chore: 오탈자 수정

--- app-main/src/main/java/net/causw/app/main/domain/campus/schedule/api/v2/dto/request/ScheduleRequest.java
+++ app-main/src/main/java/net/causw/app/main/domain/campus/schedule/api/v2/dto/request/ScheduleRequest.java
@@ -2,19 +2,19 @@ package net.causw.app.main.domain.campus.schedule.api.v2.dto.request;
 
 import java.time.LocalDateTime;
 
-import net.causw.app.main.domain.campus.schedule.entity.enums.ScheduleType;
-
 import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.AssertTrue;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 
+import net.causw.app.main.domain.campus.schedule.entity.enums.ScheduleType;
+
 @Schema(description = "일정 생성/수정 요청")
 public record ScheduleRequest(
 	@Schema(description = "일정 제목", example = "중간고사 기간", requiredMode = Schema.RequiredMode.REQUIRED) @NotBlank(message = "일정 제목은 필수입니다.") @Size(min = 1, max = 100, message = "일정 제목은 1자 이상 100자 이하여야 합니다.") String title,
 
-	@Schema(description = "일정 유형", example = "ACAEMIC", requiredMode = Schema.RequiredMode.REQUIRED) @NotNull(message = "일정 타입은 필수입니다.") ScheduleType type,
+	@Schema(description = "일정 유형", example = "ACADEMIC", requiredMode = Schema.RequiredMode.REQUIRED) @NotNull(message = "일정 타입은 필수입니다.") ScheduleType type,
 
 	@Schema(description = "일정 시작 시간", example = "2026-04-15T00:00:00", requiredMode = Schema.RequiredMode.REQUIRED) @NotNull(message = "시작 시간은 필수입니다.") LocalDateTime start,
 
