8cbd405c997f3688170b0936b83e142b21d3f517
sopp1313 <dldpdms0131@gmail.com>
2026-02-27 18:40:32 +0900

    refactor: enum 삭제 대신 deprecated처리

--- app-main/src/main/java/net/causw/app/main/domain/community/report/enums/ReportReason.java
+++ app-main/src/main/java/net/causw/app/main/domain/community/report/enums/ReportReason.java
@@ -1,6 +1,9 @@
 package net.causw.app.main.domain.community.report.enums;
 
 public enum ReportReason {
+	/** @deprecated 기존 DB 데이터 하위 호환 유지용. 신규 신고에는 사용하지 않음. */
+	@Deprecated
+	OFF_TOPIC("주제와 무관"),
 	SPAM_AD("낚시/놀림/도배"),
 	ABUSE_LANGUAGE("욕설/비하"),
 	COMMERCIAL_AD("상업적 광고 및 판매"),
