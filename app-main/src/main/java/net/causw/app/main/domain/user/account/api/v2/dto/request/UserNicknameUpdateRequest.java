3c18c699dd842966f742b3e0b1e79cfff7ac420c
sopp1313 <dldpdms0131@gmail.com>
2026-02-28 01:20:54 +0900

    feat: 글자수 제한 추가

--- app-main/src/main/java/net/causw/app/main/domain/user/account/api/v2/dto/request/UserNicknameUpdateRequest.java
+++ app-main/src/main/java/net/causw/app/main/domain/user/account/api/v2/dto/request/UserNicknameUpdateRequest.java
@@ -2,7 +2,11 @@ package net.causw.app.main.domain.user.account.api.v2.dto.request;
 
 import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 
 public record UserNicknameUpdateRequest(
-	@NotBlank(message = "닉네임을 입력해 주세요.") @Schema(description = "변경할 닉네임", example = "새닉네임") String nickname) {
+	@NotBlank(message = "닉네임을 입력해 주세요.")
+	@Size(max = 8, message = "닉네임은 8자 이하로 입력해 주세요.")
+	@Schema(description = "변경할 닉네임", example = "새닉네임")
+	String nickname) {
 }
