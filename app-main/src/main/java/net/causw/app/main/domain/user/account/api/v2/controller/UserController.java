4bd7b91ddb9f05bca177687dbc1ad2cbe0bf94d9
sopp1313 <dldpdms0131@gmail.com>
2026-03-05 22:54:40 +0900

    style: spotless

--- app-main/src/main/java/net/causw/app/main/domain/user/account/api/v2/controller/UserController.java
+++ app-main/src/main/java/net/causw/app/main/domain/user/account/api/v2/controller/UserController.java
@@ -9,13 +9,13 @@ import org.springframework.web.bind.annotation.CookieValue;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RequestPart;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.multipart.MultipartFile;
 
 import net.causw.app.main.domain.user.account.api.v1.dto.UserFcmTokenResponseDto;
--- app-main/src/main/java/net/causw/app/main/domain/user/account/api/v2/dto/request/UserNicknameUpdateRequest.java
+++ app-main/src/main/java/net/causw/app/main/domain/user/account/api/v2/dto/request/UserNicknameUpdateRequest.java
@@ -1,13 +1,13 @@
 package net.causw.app.main.domain.user.account.api.v2.dto.request;
 
+import net.causw.global.constant.StaticValue;
+
 import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
-import net.causw.global.constant.StaticValue;
 
 public record UserNicknameUpdateRequest(
-	@NotBlank(message = "닉네임을 입력해 주세요.")
-	@Size(max = StaticValue.NICKNAME_MAX_LENGTH, message = "닉네임은 " + StaticValue.NICKNAME_MAX_LENGTH + "자 이하로 입력해 주세요.")
-	@Schema(description = "변경할 닉네임", example = "새닉네임")
-	String nickname) {
+	@NotBlank(message = "닉네임을 입력해 주세요.") @Size(max = StaticValue.NICKNAME_MAX_LENGTH, message = "닉네임은 "
+		+ StaticValue.NICKNAME_MAX_LENGTH
+		+ "자 이하로 입력해 주세요.") @Schema(description = "변경할 닉네임", example = "새닉네임") String nickname) {
 }
