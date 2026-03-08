c06d6202a5e99411fd18c0aa4c645a40856c8731
hyungin0505 <hyungin0505@naver.com>
2026-03-09 02:23:56 +0900

    fix: 대표 프로젝트 응답 dto 스키마 수정

--- app-main/src/main/java/net/causw/app/main/domain/user/account/api/v2/dto/response/UserProjectDto.java
+++ app-main/src/main/java/net/causw/app/main/domain/user/account/api/v2/dto/response/UserProjectDto.java
@@ -5,15 +5,15 @@ import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 
 public record UserProjectDto(
-	@Schema(description = "사용자 경력 사항 id (null이면 새 경력 사항)", example = "uuid 형식의 String 값입니다") String id,
+	@Schema(description = "사용자 대표 프로젝트 id (null이면 새 대표 프로젝트)", example = "uuid 형식의 String 값입니다") String id,
 
-	@Schema(description = "경력 사항 시작 년도", example = "2025") Integer startYear,
+	@Schema(description = "대표 프로젝트 시작 년도", example = "2025") Integer startYear,
 
-	@Schema(description = "경력 사항 시작 월", example = "3") Integer startMonth,
+	@Schema(description = "대표 프로젝트 시작 월", example = "3") Integer startMonth,
 
-	@Schema(description = "경력 사항 종료 년도", example = "2026") Integer endYear,
+	@Schema(description = "대표 프로젝트 종료 년도", example = "2026") Integer endYear,
 
-	@Schema(description = "경력 사항 종료 월", example = "12") Integer endMonth,
+	@Schema(description = "대표 프로젝트 종료 월", example = "12") Integer endMonth,
 
 	@NotNull @Size(max = 50, message = "최대 글자 수 50을 초과했습니다.") String description) {
 }
