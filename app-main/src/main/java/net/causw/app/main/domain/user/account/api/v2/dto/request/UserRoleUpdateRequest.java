0d71af4812b7d469a0e8165dfe11c50a605f5e30
ygcho <ygcho21@gmail.com>
2026-03-10 01:45:34 +0900

    chore: RequestDTO 명세 단순화

--- app-main/src/main/java/net/causw/app/main/domain/user/account/api/v2/dto/request/UserRoleUpdateRequest.java
+++ app-main/src/main/java/net/causw/app/main/domain/user/account/api/v2/dto/request/UserRoleUpdateRequest.java
@@ -7,7 +7,7 @@ import jakarta.validation.constraints.NotNull;
 
 @Schema(description = "회원 권한 변경 요청")
 public record UserRoleUpdateRequest(
-	@Schema(description = "현재 권한", example = "COMMON", requiredMode = Schema.RequiredMode.REQUIRED) @NotNull(message = "현재 권한을 입력해 주세요.") Role currentRole,
+	@Schema(description = "현재 권한", example = "COMMON") @NotNull(message = "현재 권한을 입력해 주세요.") Role currentRole,
 
-	@Schema(description = "변경할 권한", example = "ADMIN", requiredMode = Schema.RequiredMode.REQUIRED) @NotNull(message = "변경할 권한을 입력해 주세요.") Role newRole) {
+	@Schema(description = "변경할 권한", example = "ADMIN") @NotNull(message = "변경할 권한을 입력해 주세요.") Role newRole) {
 }
