072089e5b698d0d1fa58f7f0215262f0db4073c5
hyungin0505 <hyungin0505@naver.com>
2026-03-10 00:18:19 +0900

    feat: 동문 수첩 리스트 요청 dto 검색어 길이 제한 추가

--- app-main/src/main/java/net/causw/app/main/domain/user/account/api/v2/dto/request/UserInfoListRequest.java
+++ app-main/src/main/java/net/causw/app/main/domain/user/account/api/v2/dto/request/UserInfoListRequest.java
@@ -3,10 +3,11 @@ package net.causw.app.main.domain.user.account.api.v2.dto.request;
 import java.util.List;
 
 import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.validation.constraints.Size;
 
 @Schema(description = "동문 수첩 프로필 리스트 조회 요청")
 public record UserInfoListRequest(
-	@Schema(description = "검색 키워드 (이름, 직업, 경력)", example = "검색어") String keyword,
+	@Size(max = 50, message = "검색어는 최대 50자까지 입력 가능합니다.") @Schema(description = "검색 키워드 (이름, 직업, 경력)", example = "검색어") String keyword,
 
 	@Schema(description = "학번 범위 시작", example = "1972") Integer admissionYearStart,
 
