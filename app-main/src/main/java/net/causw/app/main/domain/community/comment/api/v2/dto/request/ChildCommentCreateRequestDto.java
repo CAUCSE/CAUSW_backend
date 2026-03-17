16b1f1242a48da97a011f3d23f30bb420a58da6b
ImJaewook <bigthinking33@gmail.com>
2026-03-17 22:32:00 +0900

    feat: delete @size at id

--- app-main/src/main/java/net/causw/app/main/domain/community/comment/api/v2/dto/request/ChildCommentCreateRequestDto.java
+++ app-main/src/main/java/net/causw/app/main/domain/community/comment/api/v2/dto/request/ChildCommentCreateRequestDto.java
@@ -8,7 +8,7 @@ import jakarta.validation.constraints.Size;
 public record ChildCommentCreateRequestDto(
 	@NotBlank(message = "대댓글 내용을 입력해 주세요.") @Size(max = 255, message = "대댓글 내용은 255자를 초과할 수 없습니다.") String content,
 
-	@NotBlank(message = "답변할 댓글을 선택해 주세요.") @Size(max = 255, message = "선택된 댓글이 유효하지 않은 댓글입니다") String parentCommentId,
+	@NotBlank(message = "답변할 댓글을 선택해 주세요.") String parentCommentId,
 
 	@Schema(description = "익명글 여부", example = "False") @NotNull(message = "익명 여부가 선택되지 않았습니다.") Boolean isAnonymous) {
 }
