6f5cd7fac4112bace83f7cf83900f1173588bc91
ygcho <ygcho21@gmail.com>
2026-04-01 16:33:38 +0900

    fix: 약관 조회 비인증 허용

--- app-main/src/main/java/net/causw/app/main/core/security/WebSecurityConfig.java
+++ app-main/src/main/java/net/causw/app/main/core/security/WebSecurityConfig.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.annotation.Order;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -73,6 +74,7 @@ public class WebSecurityConfig {
 				.requestMatchers("/api/v2/auth/**", "/oauth2/**", "/login/oauth2/**",
 					"/api/v2/users/check-nickname", "/api/v2/users/check-phone")
 				.permitAll()
+				.requestMatchers(HttpMethod.GET, "/api/v2/terms").permitAll()
 				.requestMatchers("/api/v2/admin/**").hasRole("ADMIN")
 				.anyRequest().authenticated())
 			.oauth2Login(oauth2 -> oauth2
