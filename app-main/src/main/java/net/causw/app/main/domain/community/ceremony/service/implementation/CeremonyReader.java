ff38282e8ff44d0f5d2d45c2a160e124534dbd17
CHAEYEON LEE <102214015+chaeyeonlee898@users.noreply.github.com>
2026-04-06 10:09:32 +0900

    feat: 미처리 경조사 수 조회 api 구현 (#1199)
    * feat: 미처리 경조사 수 조회 api 구현
    
    * fix: 메서드명 간결하게 수정
    
    * fix: 미처리 경조사 수 조회 전용 메서드로 수정

--- app-main/src/main/java/net/causw/app/main/domain/community/ceremony/service/implementation/CeremonyReader.java
+++ app-main/src/main/java/net/causw/app/main/domain/community/ceremony/service/implementation/CeremonyReader.java
@@ -53,4 +53,8 @@ public class CeremonyReader {
 		Pageable pageable) {
 		return ceremonyQueryRepository.findAllForAdmin(fromDate, toDate, state, pageable);
 	}
+
+	public long countAwaitCeremony() {
+		return ceremonyRepository.countByCeremonyState(CeremonyState.AWAIT);
+	}
 }
