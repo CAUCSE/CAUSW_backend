3e0ce7832b65d0639f70654e0495301132783e29
bingle625 <fhqhxm8899@gmail.com>
2026-04-08 16:10:54 +0900

    feat: constraint 순서 enum 클래스에 맞게 변경

--- app-main/src/main/resources/db/migration/V20260408153150__modifyNoticeTypeConstraint.sql
+++ app-main/src/main/resources/db/migration/V20260408153150__modifyNoticeTypeConstraint.sql
@@ -1,3 +1,3 @@
 -- Migration: modifyNoticeTypeConstraint
 alter table tb_notification
-    modify notice_type enum ('POST', 'COMMENT', 'CEREMONY', 'BOARD', 'ADMISSION', 'COMMUNITY', 'SYSTEM', 'OFFICIAL', 'CEREMONY_V2') null;
+    modify notice_type enum ('POST', 'COMMENT', 'BOARD', 'ADMISSION', 'CEREMONY', 'COMMUNITY', 'SYSTEM', 'OFFICIAL', 'CEREMONY_V2') null;
