bad696ea465e1d2a54c8b8991ae031032b24b7fd
ygcho <ygcho21@gmail.com>
2026-04-11 23:00:55 +0900

    chore: 상수 제거

--- app-main/src/main/java/net/causw/app/main/domain/user/account/api/v2/dto/request/UserRegistrationRequest.java
+++ app-main/src/main/java/net/causw/app/main/domain/user/account/api/v2/dto/request/UserRegistrationRequest.java
@@ -1,7 +1,5 @@
 package net.causw.app.main.domain.user.account.api.v2.dto.request;
 
-import net.causw.global.constant.StaticValue;
-
 import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Pattern;
@@ -12,7 +10,5 @@ public record UserRegistrationRequest(
 
 	@NotBlank(message = "연락처를 입력해 주세요.") @Schema(description = "연락처", example = "010-1234-5678", requiredMode = Schema.RequiredMode.REQUIRED) @Pattern(regexp = "^01(?:0|1|[6-9])-(\\d{3}|\\d{4})-\\d{4}$", message = "전화번호 형식에 맞지 않습니다.") String phoneNumber,
 
-	@NotBlank(message = "닉네임을 입력해 주세요.") @Size(max = StaticValue.NICKNAME_MAX_LENGTH, message = "닉네임은 "
-		+ StaticValue.NICKNAME_MAX_LENGTH
-		+ "자 이하여야 합니다.") @Schema(description = "닉네임", example = "푸앙") String nickname) {
+	@NotBlank(message = "닉네임을 입력해 주세요.") @Size(max = 8, message = "닉네임은 8자 이하여야 합니다.") @Schema(description = "닉네임", example = "푸앙") String nickname) {
 }
--- app-main/src/main/java/net/causw/app/main/domain/user/auth/api/v2/dto/request/EmailSignupRequest.java
+++ app-main/src/main/java/net/causw/app/main/domain/user/auth/api/v2/dto/request/EmailSignupRequest.java
@@ -1,7 +1,6 @@
 package net.causw.app.main.domain.user.auth.api.v2.dto.request;
 
 import net.causw.app.main.domain.user.account.policy.PasswordPolicy;
-import net.causw.global.constant.StaticValue;
 
 import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.NotBlank;
@@ -12,15 +11,13 @@ import jakarta.validation.constraints.Size;
 public record EmailSignupRequest(
 	@NotBlank(message = "이메일을 입력해 주세요.") @Pattern(regexp = "^[a-zA-Z0-9_!#$%&'*+/=?`{|}~.-]+@[a-zA-Z0-9.-]+$", message = "이메일 형식에 맞지 않습니다.") @Schema(description = "이메일", example = "user@cau.ac.kr") String email,
 
-	@NotBlank(message = "비밀번호를 입력해 주세요.") @Pattern(regexp = PasswordPolicy.REGEX, message = PasswordPolicy.VALIDATION_MESSAGE) @Schema(description = "비밀번호", example = "password00!!") String password,
+	@NotBlank(message = "비밀번호를 입력해 주세요.") @Pattern(regexp = PasswordPolicy.REGEX, message = PasswordPolicy.VALIDATION_MESSAGE) @Schema(description = "비밀번호", example = "abcdefg12!") String password,
 
 	@NotBlank(message = "이름을 입력해 주세요.") @Size(max = 20, message = "이름은 20자 이하로 입력해 주세요.") @Schema(description = "이름 (본명)", example = "홍길동") String name,
 
 	@NotBlank(message = "연락처를 입력해 주세요.") @Schema(description = "연락처", example = "010-1234-5678", requiredMode = Schema.RequiredMode.REQUIRED) @Pattern(regexp = "^01(?:0|1|[6-9])-(\\d{3}|\\d{4})-\\d{4}$", message = "전화번호 형식에 맞지 않습니다.") String phoneNumber,
 
-	@NotBlank(message = "닉네임을 입력해 주세요.") @Size(max = StaticValue.NICKNAME_MAX_LENGTH, message = "닉네임은 "
-		+ StaticValue.NICKNAME_MAX_LENGTH
-		+ "자 이하로 입력해 주세요.") @Schema(description = "닉네임", example = "푸앙") String nickname,
+	@NotBlank(message = "닉네임을 입력해 주세요.") @Size(max = 8, message = "닉네임은 8자 이하로 입력해 주세요.") @Schema(description = "닉네임", example = "푸앙") String nickname,
 
 	@NotBlank(message = "인증번호를 입력해 주세요.") @Schema(description = "인증확인한 인증번호", example = "1234AB") String emailVerificationCode) {
 }
