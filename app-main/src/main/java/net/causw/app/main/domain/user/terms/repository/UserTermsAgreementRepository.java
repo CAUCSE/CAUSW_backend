2daeff547498487c6ba63adf44e7162167a2b050
ygcho <ygcho21@gmail.com>
2026-04-13 16:06:28 +0900

    style: spotless

--- app-main/src/main/java/net/causw/app/main/domain/user/terms/repository/UserTermsAgreementRepository.java
+++ app-main/src/main/java/net/causw/app/main/domain/user/terms/repository/UserTermsAgreementRepository.java
@@ -1,7 +1,6 @@
 package net.causw.app.main.domain.user.terms.repository;
 
 import java.util.List;
-import java.util.Set;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
--- app-main/src/main/java/net/causw/app/main/domain/user/terms/service/implementation/UserTermsAgreementReader.java
+++ app-main/src/main/java/net/causw/app/main/domain/user/terms/service/implementation/UserTermsAgreementReader.java
@@ -1,7 +1,6 @@
 package net.causw.app.main.domain.user.terms.service.implementation;
 
 import java.util.List;
-import java.util.Set;
 
 import org.springframework.stereotype.Component;
 
