548b501653a9555835554710e03ef34ab5bc5159
wldy4627 <wldy4627@gmail.com>
2026-04-13 15:19:04 +0900

    style: spotless 포맷팅 (#1213)

--- app-main/src/test/java/net/causw/app/main/domain/user/account/service/UserAccountServiceIT.java
+++ app-main/src/test/java/net/causw/app/main/domain/user/account/service/UserAccountServiceIT.java
@@ -9,8 +9,6 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.data.redis.core.RedisTemplate;
-import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.transaction.annotation.Transactional;
 
 import net.causw.app.main.domain.asset.locker.service.v2.implementation.LockerReader;
