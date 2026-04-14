6a3858ea551245cd94e389b5cbff8b0019f84cc8
AhnPilOn <88488962+KEEKE132@users.noreply.github.com>
2026-04-14 18:27:00 +0900

    feat: 온보딩 이메일 인증 api 개발 (#1188)
    * feat: v2 온보딩 이메일 인증 개발
    
    * feat: is_email_verified 컬럼 추가
    
    * feat: is_email_verified 컬럼 추가
    
    * feat: 온보딩 이메일 인가 검사
    
    * feat: 온보딩 메일전송시 유저 상태 검사
    
    * feat: EmailVerfication Enum 스크립트도 추가

--- app-main/src/main/java/net/causw/app/main/domain/user/account/entity/user/User.java
+++ app-main/src/main/java/net/causw/app/main/domain/user/account/entity/user/User.java
@@ -1,6 +1,6 @@
 package net.causw.app.main.domain.user.account.entity.user;
 
-import static net.causw.global.constant.StaticValue.*;
+import static net.causw.global.constant.StaticValue.NO_PHONE_NUMBER_MESSAGE;
 
 import java.time.LocalDateTime;
 import java.util.HashSet;
@@ -163,6 +163,10 @@ public class User extends BaseEntity {
 	@Builder.Default
 	private Integer reportCount = 0;
 
+	@Column(name = "is_email_verified", nullable = false)
+	@Builder.Default
+	private Boolean isEmailVerified = false;
+
 	public void delete() {
 		this.email = "deleted_" + this.getId();
 		this.name = "탈퇴한 사용자";
@@ -243,6 +247,7 @@ public class User extends BaseEntity {
 			.phoneNumber(dto.phoneNumber())
 			.agreements(TermAgreements.createRequiredAgreements())
 			.isV2(true)
+			.isEmailVerified(true)
 			.build();
 	}
 
@@ -422,6 +427,14 @@ public class User extends BaseEntity {
 	// 신고 관련 메소드
 	public void increaseReportCount() {
 		this.reportCount++;
+	}
+
+	public void markEmailAsVerified() {
+		this.isEmailVerified = true;
+	}
+
+	public boolean checkEmailVerification() {
+		return this.isEmailVerified;
 	}
 
 	@Override
