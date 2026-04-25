247caa10499238ae1f26591e1573e19075784919
ygcho <ygcho21@gmail.com>
2026-04-25 16:13:33 +0900

    chore: v1 dto 원복

--- app-main/src/main/java/net/causw/app/main/domain/user/account/api/v1/dto/UserInfoUpdateRequestDto.java
+++ app-main/src/main/java/net/causw/app/main/domain/user/account/api/v1/dto/UserInfoUpdateRequestDto.java
@@ -16,7 +16,7 @@ public class UserInfoUpdateRequestDto {
 	@Pattern(regexp = "^01(?:0|1|[6-9])-(\\d{3}|\\d{4})-\\d{4}$", message = "전화번호 형식에 맞지 않습니다.")
 	private String phoneNumber;
 
-	@Size(max = 100, message = "최대 글자수 100을 초과했습니다.")
+	@Size(max = 200, message = "최대 글자수 200을 초과했습니다.")
 	private String description;
 
 	@Size(max = 50, message = "최대 글자수 50을 초과했습니다.")
