3d751e706a9bad42151ea3a97848c9964223d340
ygcho <ygcho21@gmail.com>
2026-04-28 20:48:50 +0900

    fix: 누락된 import문 추가

--- app-main/src/main/java/net/causw/app/main/domain/user/auth/api/v2/dto/request/EmailSignupRequest.java
+++ app-main/src/main/java/net/causw/app/main/domain/user/auth/api/v2/dto/request/EmailSignupRequest.java
@@ -5,6 +5,7 @@ import java.util.List;
 import net.causw.app.main.domain.user.account.policy.PasswordPolicy;
 
 import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.Pattern;
