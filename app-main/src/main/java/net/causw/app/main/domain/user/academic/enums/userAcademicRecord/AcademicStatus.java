b183966f7bb57f39b881ef1fd652baefff5a3de7
ImJaewook <bigthinking33@gmail.com>
2026-04-30 21:48:38 +0900

    refactor: add @Deprecated to AcademicStatus

--- app-main/src/main/java/net/causw/app/main/domain/user/academic/enums/userAcademicRecord/AcademicStatus.java
+++ app-main/src/main/java/net/causw/app/main/domain/user/academic/enums/userAcademicRecord/AcademicStatus.java
@@ -9,13 +9,22 @@ import lombok.Getter;
 @AllArgsConstructor
 public enum AcademicStatus {
 	ENROLLED("재학"), // 재학
-	LEAVE_OF_ABSENCE("휴학"), // 휴학
 	GRADUATED("졸업"), // 졸업
+	UNDETERMINED("미정"), // 미정 (학적상태가 인증 필요)
+	/**
+	 * @deprecated V1에서 사용하던 status "휴학", "중퇴", "정학", "퇴학", "교수"는 V2에서 사용되지 않음
+	 */
+	@Deprecated
+	LEAVE_OF_ABSENCE("휴학"), // 휴학
+	@Deprecated
 	DROPPED_OUT("중퇴"), // 중퇴
+	@Deprecated
 	SUSPEND("정학"), // 정학
+	@Deprecated
 	EXPEL("퇴학"), // 퇴학
-	PROFESSOR("교수"), // 교수
-	UNDETERMINED("미정"); // 미정 (학적상태가 인증 필요)
+	@Deprecated
+	PROFESSOR("교수"); // 교수
+
 
 	private final String value;
 
