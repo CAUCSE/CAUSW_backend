fa730d4c9d2eb7805b1ce8ea79eee6e65509280a
JAEWOOK IM <81840905+glucosei@users.noreply.github.com>
2026-05-03 17:18:39 +0900

    feat: 게시글 작성 가능한 게시판 목록 조회 API 개발 (#1281)
    * feat: define dto
    
    * feat: implement query and Reader
    
    * feat: implement Service and Controller
    
    * refactor: spotless apply
    
    * fix: change dto description
    
    * refactor: change BoardService expression
    
    * refactor: add @Deprecated to AcademicStatus
    
    * fix: change query logic left join->exist, add isDeleted check and order
    
    * refactor: spotlessApply

--- app-main/src/main/java/net/causw/app/main/domain/community/board/service/v2/BoardService.java
+++ app-main/src/main/java/net/causw/app/main/domain/community/board/service/v2/BoardService.java
@@ -9,6 +9,8 @@ import net.causw.app.main.domain.community.board.entity.Board;
 import net.causw.app.main.domain.community.board.service.implementation.BoardConfigReader;
 import net.causw.app.main.domain.community.board.service.implementation.BoardReader;
 import net.causw.app.main.domain.community.board.service.v2.dto.BoardReadableItemResult;
+import net.causw.app.main.domain.community.board.service.v2.dto.BoardWritableItemResult;
+import net.causw.app.main.domain.user.academic.enums.userAcademicRecord.AcademicStatus;
 import net.causw.app.main.domain.user.account.service.implementation.UserReader;
 
 import lombok.RequiredArgsConstructor;
@@ -38,6 +40,28 @@ public class BoardService {
 		List<Board> boards = boardReader.findAllByIdsNotDeleted(boardIds);
 		return boards.stream()
 			.map(b -> new BoardReadableItemResult(b.getId(), b.getName()))
+			.toList();
+	}
+
+	/**
+	 * 특정 사용자의 쓰기 권한이 있는 게시판의 id, name 목록을 표시 순서대로 반환합니다.
+	 * 사용자의 학적 상태가 졸업 또는 재학일 경우만 해당됩니다.
+	 *
+	 * @param userId 사용자 ID
+	 * @return 쓰기 가능한 게시판 목록 (id, name)
+	 */
+	public List<BoardWritableItemResult> getWritableBoards(String userId) {
+
+		AcademicStatus academicStatus = userReader.findUserById(userId).getAcademicStatus();
+		//졸업, 재학 academicStatus만 허용
+		if (academicStatus != AcademicStatus.ENROLLED && academicStatus != AcademicStatus.GRADUATED) {
+			return List.of();
+		}
+
+		List<Board> boards = boardConfigReader.getWritableBoardIdsByUserId(userId);
+
+		return boards.stream()
+			.map(b -> new BoardWritableItemResult(b.getId(), b.getName()))
 			.toList();
 	}
 }
