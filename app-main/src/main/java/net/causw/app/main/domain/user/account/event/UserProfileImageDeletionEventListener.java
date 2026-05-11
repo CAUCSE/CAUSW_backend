e4ae1d00a282ea6cf6fff5eef358be928d07f88a
wldy4627 <wldy4627@gmail.com>
2026-05-12 01:44:25 +0900

    feat: 탈퇴 후 프로필 이미지 삭제 이벤트 리스너 추가 (#1285)

---
+++ app-main/src/main/java/net/causw/app/main/domain/user/account/event/UserProfileImageDeletionEventListener.java
@@ -0,0 +1,29 @@
+package net.causw.app.main.domain.user.account.event;
+
+import org.springframework.stereotype.Component;
+import org.springframework.transaction.event.TransactionPhase;
+import org.springframework.transaction.event.TransactionalEventListener;
+
+import net.causw.app.main.shared.storage.v2.StorageClient;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@Component
+@RequiredArgsConstructor
+public class UserProfileImageDeletionEventListener {
+
+	private final StorageClient storageClient;
+
+	@TransactionalEventListener(phase = TransactionPhase.AFTER_COMMIT)
+	public void deleteProfileImageFile(UserProfileImageDeletionRequestedEvent event) {
+		try {
+			storageClient.delete(event.fileKey());
+
+			log.info("[User Withdraw] 프로필 이미지 파일 삭제 완료. fileKey: {}", event.fileKey());
+		} catch (Exception e) {
+			log.error("[User Withdraw] 프로필 이미지 파일 삭제 실패. fileKey: {}", event.fileKey(), e);
+		}
+	}
+}
