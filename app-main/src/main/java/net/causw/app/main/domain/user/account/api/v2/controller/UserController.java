835850c7db53e54e08754137dbd5c454b9c1ccf8
chaeyeonlee898 <chaeyeonlee898@gmail.com>
2026-05-14 22:59:15 +0900

    chore: spotlessApply 반영

--- app-main/src/main/java/net/causw/app/main/domain/user/account/api/v2/controller/UserController.java
+++ app-main/src/main/java/net/causw/app/main/domain/user/account/api/v2/controller/UserController.java
@@ -6,7 +6,6 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.bind.annotation.CookieValue;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
