017a1747f769f68779ab234bed0441d8f6f7e8d2
wldy4627 <wldy4627@gmail.com>
2026-05-15 01:52:35 +0900

    refactor: 자진 탈퇴 복구 검증 조건 강화 (#1284)

--- app-main/src/main/java/net/causw/app/main/domain/user/account/service/UserAdminService.java
+++ app-main/src/main/java/net/causw/app/main/domain/user/account/service/UserAdminService.java
@@ -145,15 +145,9 @@ public class UserAdminService {
 	}
 
 	// 자진 탈퇴 사용자 복원이 가능한지 검증
-	// - DROP 상태가 아니어야 하며, 실제로 탈퇴(isDeleted) 상태여야 함
+	// - INACTIVE 상태이며 실제로 탈퇴(isDeleted) 상태여야 함
 	private void validateRestorableWithdrawnUser(User targetUser) {
-		// 1. 추방(DROP)된 유저는 이 API를 통해 복구할 수 없음 (restoreUser API 사용 유도)
-		if (targetUser.getState() == UserState.DROP) {
-			throw UserErrorCode.USER_NOT_RESTORABLE.toBaseException();
-		}
-
-		// 2. 실제로 탈퇴(isDeleted) 상태인 유저여야 함
-		if (!targetUser.isDeleted()) {
+		if (!targetUser.isInactive() || !targetUser.isDeleted()) {
 			throw UserErrorCode.USER_NOT_RESTORABLE.toBaseException();
 		}
 	}
