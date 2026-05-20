fec3e3944dfc1a6d56f0316f56d77a9c6de6149b
ImJaewook <bigthinking33@gmail.com>
2026-05-21 08:31:00 +0900

    refactor: add specific comment to migration script

--- app-main/src/main/resources/db/migration/V20260520163901__MigrateDeprecatedAcademicStatus.sql
+++ app-main/src/main/resources/db/migration/V20260520163901__MigrateDeprecatedAcademicStatus.sql
@@ -5,5 +5,7 @@ SET academic_status = 'ENROLLED'
 WHERE academic_status IN ('LEAVE_OF_ABSENCE', 'DROPPED_OUT', 'SUSPEND', 'EXPEL', 'PROFESSOR');
 
 -- user_academic_record_log 테이블에서 행 삭제
+-- 이력 기록 테이블 특성상 과거의 기록을 수정하는 것이 적절치 않고,
+-- DB에 'LEAVE_OF_ABSENCE'로 바꾸는 기록만 있어서 기록을 삭제하는 것이 적절하다고 판단함
 DELETE FROM tb_user_academic_record_log
 WHERE prior_academic_record_application_status IN ('LEAVE_OF_ABSENCE', 'DROPPED_OUT', 'SUSPEND', 'EXPEL', 'PROFESSOR');
