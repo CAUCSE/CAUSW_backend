15f405bde615904c895d1beb795a05dbfcf6e8bf
riadan710 <riadan710@gmail.com>
2026-05-23 21:41:47 +0900

    refactor: 크롤링 게시판 명칭 변경 및 flyway 마이그레이션 스크립트 추가

--- app-main/src/main/resources/db/migration/V20260520204117__AddOfficialProfileToBoardConfig.sql
+++
@@ -1,5 +0,0 @@
--- Migration: AddOfficialProfileToBoardConfig
-
-ALTER TABLE tb_board_config
-ADD COLUMN official_nickname VARCHAR(255) DEFAULT NULL,
-ADD COLUMN official_profile_image_id VARCHAR(255) DEFAULT NULL;
