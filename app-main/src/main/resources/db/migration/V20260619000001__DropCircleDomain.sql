f1551d57bd8acc9f7a3517e05c29ac6065fc9560
bingle625 <fhqhxm8899@gmail.com>
2026-06-19 16:31:59 +0900

    fix: circle fk 삭제 마이그레이션

--- app-main/src/main/resources/db/migration/V20260619000001__DropCircleDomain.sql
+++ app-main/src/main/resources/db/migration/V20260619000001__DropCircleDomain.sql
@@ -1,6 +1,10 @@
 ALTER TABLE tb_board
+    DROP FOREIGN KEY FKf56nd4y1y3jqyec9a19gl4i43;
+ALTER TABLE tb_board
     DROP COLUMN circle_id;
 
+ALTER TABLE tb_form
+    DROP FOREIGN KEY FKsihc207xm6hj4jd97lvyixgvg;
 ALTER TABLE tb_form
     DROP COLUMN circle_id;
 
