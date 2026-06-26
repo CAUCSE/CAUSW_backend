b00eb55af04c5b001d09f45552f84aebdd1535b5
bingle625 <fhqhxm8899@gmail.com>
2026-06-26 15:09:26 +0900

    feat: 대댓글 서비스와 전용 커맨드 제거

--- app-main/src/main/java/net/causw/app/main/domain/community/comment/api/v2/mapper/ChildCommentResponseDtoMapper.java
+++ app-main/src/main/java/net/causw/app/main/domain/community/comment/api/v2/mapper/ChildCommentResponseDtoMapper.java
@@ -3,6 +3,7 @@ package net.causw.app.main.domain.community.comment.api.v2.mapper;
 import net.causw.app.main.domain.community.comment.api.v2.dto.response.ChildCommentResponseDto;
 import net.causw.app.main.domain.community.comment.service.dto.ChildCommentResult;
 import net.causw.app.main.domain.community.comment.service.dto.CommentAuthorInfo;
+import net.causw.app.main.domain.community.comment.service.dto.CommentResult;
 
 import lombok.AccessLevel;
 import lombok.NoArgsConstructor;
@@ -29,6 +30,29 @@ public class ChildCommentResponseDtoMapper {
 			.isAnonymous(author.isAnonymous())
 			.isOwner(author.isOwner())
 			.isChildCommentLike(result.isChildCommentLike())
+			.numLike(result.numLike())
+			.build();
+	}
+
+	public static ChildCommentResponseDto toResponseDto(CommentResult result) {
+		CommentAuthorInfo author = result.authorInfo();
+		return ChildCommentResponseDto.builder()
+			.id(result.id())
+			.content(result.content())
+			.isDeleted(result.isDeleted())
+			.createdAt(result.createdAt())
+			.updatedAt(result.updatedAt())
+			.writerName(author.writerName())
+			.writerNickname(author.writerNickname())
+			.displayWriterNickname(author.displayWriterNickname())
+			.writerAdmissionYear(author.writerAdmissionYear())
+			.writerProfileImage(author.writerProfileImage())
+			.updatable(author.updatable())
+			.deletable(author.deletable())
+			.isBlocked(author.isBlocked())
+			.isAnonymous(author.isAnonymous())
+			.isOwner(author.isOwner())
+			.isChildCommentLike(result.isCommentLike())
 			.numLike(result.numLike())
 			.build();
 	}
---
+++ app-main/src/test/java/net/causw/app/main/domain/community/comment/api/v2/controller/ChildCommentControllerTest.java
@@ -0,0 +1,116 @@
+package net.causw.app.main.domain.community.comment.api.v2.controller;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.BDDMockito.given;
+import static org.mockito.BDDMockito.then;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.ArgumentCaptor;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import net.causw.app.main.domain.community.comment.api.v2.dto.request.ChildCommentCreateRequestDto;
+import net.causw.app.main.domain.community.comment.api.v2.dto.request.ChildCommentUpdateRequestDto;
+import net.causw.app.main.domain.community.comment.api.v2.dto.response.ChildCommentResponseDto;
+import net.causw.app.main.domain.community.comment.service.CommentService;
+import net.causw.app.main.domain.community.comment.service.dto.CommentAuthorInfo;
+import net.causw.app.main.domain.community.comment.service.dto.CommentCreateCommand;
+import net.causw.app.main.domain.community.comment.service.dto.CommentResult;
+import net.causw.app.main.domain.community.comment.service.dto.CommentUpdateCommand;
+import net.causw.app.main.domain.user.auth.userdetails.CustomUserDetails;
+import net.causw.app.main.shared.dto.ApiResponse;
+
+@ExtendWith(MockitoExtension.class)
+class ChildCommentControllerTest {
+
+	@InjectMocks
+	private ChildCommentController childCommentController;
+
+	@Mock
+	private CommentService commentService;
+
+	@Mock
+	private CustomUserDetails userDetails;
+
+	@Nested
+	@DisplayName("대댓글 생성")
+	class CreateChildComment {
+
+		@Test
+		@DisplayName("통합 댓글 생성 command로 부모 댓글 ID를 전달한다")
+		void givenChildCommentRequest_whenCreate_thenDelegateToCommentService() {
+			// given
+			ChildCommentCreateRequestDto request = new ChildCommentCreateRequestDto(
+				"대댓글 내용", "parent-comment-id", false);
+			given(userDetails.getUserId()).willReturn("creator-id");
+			given(commentService.createComment(any(CommentCreateCommand.class))).willReturn(commentResult());
+			ArgumentCaptor<CommentCreateCommand> captor = ArgumentCaptor.forClass(CommentCreateCommand.class);
+
+			// when
+			ApiResponse<ChildCommentResponseDto> response = childCommentController.createChildComment(
+				request, userDetails);
+
+			// then
+			then(commentService).should().createComment(captor.capture());
+			CommentCreateCommand command = captor.getValue();
+			assertThat(command.content()).isEqualTo("대댓글 내용");
+			assertThat(command.postId()).isNull();
+			assertThat(command.parentCommentId()).isEqualTo("parent-comment-id");
+			assertThat(command.isAnonymous()).isFalse();
+			assertThat(command.creatorId()).isEqualTo("creator-id");
+			assertThat(response.getData().id()).isEqualTo("comment-id");
+			assertThat(response.getData().isChildCommentLike()).isTrue();
+		}
+	}
+
+	@Nested
+	@DisplayName("대댓글 수정")
+	class UpdateChildComment {
+
+		@Test
+		@DisplayName("통합 댓글 수정 command로 대댓글 ID를 전달한다")
+		void givenChildCommentRequest_whenUpdate_thenDelegateToCommentService() {
+			// given
+			ChildCommentUpdateRequestDto request = new ChildCommentUpdateRequestDto("수정 내용");
+			given(userDetails.getUserId()).willReturn("updater-id");
+			given(commentService.updateComment(any(CommentUpdateCommand.class))).willReturn(commentResult());
+			ArgumentCaptor<CommentUpdateCommand> captor = ArgumentCaptor.forClass(CommentUpdateCommand.class);
+
+			// when
+			ApiResponse<ChildCommentResponseDto> response = childCommentController.updateChildComment(
+				"child-comment-id", request, userDetails);
+
+			// then
+			then(commentService).should().updateComment(captor.capture());
+			CommentUpdateCommand command = captor.getValue();
+			assertThat(command.commentId()).isEqualTo("child-comment-id");
+			assertThat(command.content()).isEqualTo("수정 내용");
+			assertThat(command.updaterId()).isEqualTo("updater-id");
+			assertThat(response.getData().id()).isEqualTo("comment-id");
+		}
+	}
+
+	private CommentResult commentResult() {
+		return new CommentResult(
+			"comment-id",
+			"내용",
+			LocalDateTime.of(2026, 6, 26, 12, 0),
+			LocalDateTime.of(2026, 6, 26, 12, 1),
+			false,
+			"post-id",
+			new CommentAuthorInfo(null, null, null, null, null, false, false, false, false, false),
+			true,
+			false,
+			3L,
+			0L,
+			List.of());
+	}
+}
