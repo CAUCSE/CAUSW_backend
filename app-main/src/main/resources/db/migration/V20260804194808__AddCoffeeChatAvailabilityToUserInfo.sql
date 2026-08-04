918f2aa877402293a20669717e8063aae1c382fb
bingle625 <fhqhxm8899@gmail.com>
2026-08-04 20:25:18 +0900

    fix: typo

--- app-main/src/main/resources/db/migration/V20260804194808__AddCoffeeChatAvailabilityToUserInfo.sql
+++ app-main/src/main/resources/db/migration/V20260804194808__AddCoffeeChatAvailabilityToUserInfo.sql
@@ -2,7 +2,7 @@
 
 -- 커피챗 허용 boolean 추가
 ALTER TABLE tb_user_info
-    ADD COLUMN is_coffe_chat_available boolean not null default false;
+    ADD COLUMN is_coffee_chat_available boolean not null default false;
 
 -- 기존 전화번호 공개 null 값 default false로 변경
 UPDATE tb_user_info
