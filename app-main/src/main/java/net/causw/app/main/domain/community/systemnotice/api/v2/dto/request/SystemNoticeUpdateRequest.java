57c2061092e0489ba0a33dc5afcf694c9e1b6d7e
sopp1313 <dldpdms0131@gmail.com>
2026-08-09 03:59:16 +0900

    fix: 시스템 공지 제목 길이 제약 추가

--- app-main/src/main/java/net/causw/app/main/domain/community/systemnotice/api/v2/dto/request/SystemNoticeUpdateRequest.java
+++ app-main/src/main/java/net/causw/app/main/domain/community/systemnotice/api/v2/dto/request/SystemNoticeUpdateRequest.java
@@ -1,8 +1,9 @@
 package net.causw.app.main.domain.community.systemnotice.api.v2.dto.request;
 
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 
 public record SystemNoticeUpdateRequest(
-	@NotBlank String title,
+	@NotBlank @Size(max = 255, message = "시스템 공지 제목은 255자를 초과할 수 없습니다.") String title,
 	@NotBlank String content) {
 }
