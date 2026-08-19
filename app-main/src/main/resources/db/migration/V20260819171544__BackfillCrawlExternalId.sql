8675bed85cd96497473967179ca007c72d65f1d5
bingle625 <fhqhxm8899@gmail.com>
2026-08-19 17:35:33 +0900

    feat: 크롤링 소스 식별용 external id 백필 sql 수정

--- app-main/src/main/resources/db/migration/V20260819171544__BackfillCrawlExternalId.sql
+++ app-main/src/main/resources/db/migration/V20260819171544__BackfillCrawlExternalId.sql
@@ -1,4 +1,51 @@
 -- Migration: BackfillCrawlExternalId
+-- 동일 code와 uid를 가진 중복 공지는 가장 먼저 생성된 행만 남긴다.
+DELETE file_link
+FROM tb_crawled_file_link file_link
+JOIN tb_crawled_notice target
+    ON target.id = file_link.crawled_notice_id
+JOIN tb_crawled_notice keeper
+    ON target.site_id = 'cau-sw-notice'
+   AND keeper.site_id = 'cau-sw-notice'
+   AND CONCAT(
+        SUBSTRING_INDEX(SUBSTRING_INDEX(target.link, 'code=', -1), '&', 1),
+        ':',
+        SUBSTRING_INDEX(SUBSTRING_INDEX(target.link, 'uid=', -1), '&', 1)
+   ) = CONCAT(
+        SUBSTRING_INDEX(SUBSTRING_INDEX(keeper.link, 'code=', -1), '&', 1),
+        ':',
+        SUBSTRING_INDEX(SUBSTRING_INDEX(keeper.link, 'uid=', -1), '&', 1)
+   )
+   AND (
+        target.created_at > keeper.created_at
+        OR (target.created_at = keeper.created_at AND target.id > keeper.id)
+   );
+
+DELETE FROM tb_crawled_notice
+WHERE id IN (
+    SELECT id
+    FROM (
+        SELECT target.id
+        FROM tb_crawled_notice target
+        JOIN tb_crawled_notice keeper
+            ON target.site_id = 'cau-sw-notice'
+           AND keeper.site_id = 'cau-sw-notice'
+           AND CONCAT(
+                SUBSTRING_INDEX(SUBSTRING_INDEX(target.link, 'code=', -1), '&', 1),
+                ':',
+                SUBSTRING_INDEX(SUBSTRING_INDEX(target.link, 'uid=', -1), '&', 1)
+           ) = CONCAT(
+                SUBSTRING_INDEX(SUBSTRING_INDEX(keeper.link, 'code=', -1), '&', 1),
+                ':',
+                SUBSTRING_INDEX(SUBSTRING_INDEX(keeper.link, 'uid=', -1), '&', 1)
+           )
+           AND (
+                target.created_at > keeper.created_at
+                OR (target.created_at = keeper.created_at AND target.id > keeper.id)
+           )
+    ) AS delete_targets
+);
+
 -- 중앙대 SW 공지의 외부 식별자를 링크의 code와 uid 조합으로 변경한다.
 UPDATE tb_crawled_notice
 SET external_id = CONCAT(
